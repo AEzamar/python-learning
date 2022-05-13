5b534cf8915e1e3faf47c6cbbdbf9c9b3a217fa6
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-05-13 10:10:30 -0500

    Added a new method to the person class dded new method to Person class

--- class_practice.py
+++ class_practice.py
@@ -5,9 +5,12 @@ class Person:
         self.job = job
     def intro(self):
         print(f"Hello, my name is {self.name}, I\'m {self.age} years old and I\'m a {self.job}")
+    def occupation(self):
+        print(f"Name: {self.name} | Job: {self.job}")
 
 
 ed = Person("Eduardo", 34, "Software Developer")
 print(ed)
 ed.intro()
+ed.occupation()
 
