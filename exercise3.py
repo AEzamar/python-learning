df44c88b94fa7ce5e84d4495790f6dbfbd3726f6
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-23 09:21:35 -0500

    Added math module to handle flooring of converted weight in exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,3 +1,4 @@
+import math
 weight = int(input('What is your weight in pounds? '))
 kilograms =  weight / 2.2
-print(f'Your weight is {kilograms}')
+print(f'Your weight is {math.floor(kilograms)}')
