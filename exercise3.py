1a8ba37d113ad9b66351ec20abe16eae9878e5cd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-24 09:28:13 -0500

    Another small change to exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,4 +1,4 @@
 import math
-weight = int(input('What is your weight in pounds? '))
-kilograms =  weight / 2.2
+weight = int(input('What is your weight in lbs? '))
+kilograms =  weight * 0.45
 print(f'Your weight is {math.floor(kilograms)} kilograms!')
