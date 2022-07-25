e74ba9f72752086dcbad50328e119e239c1b8a5a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-25 06:44:48 -0500

    Re-learning basic string methods

--- basic_str.py
+++ basic_str.py
@@ -1,3 +1,9 @@
 course = 'Python for Beginners'
 print(len(course))
 print(course.lower())
+print(course.upper())
+str_arr = course.split()
+print(str_arr)
+
+for word in str_arr:
+    print(word[0].upper() + word[1:])
