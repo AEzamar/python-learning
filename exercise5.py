867ecd23aa27157d5b89880a9fbdb471805f26b7
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-27 10:59:32 -0500

    Sumple string format change in script

--- exercise5.py
+++ exercise5.py
@@ -1,6 +1,6 @@
 house_price = 1000000
 good_credit = True
 if(good_credit):
-    print(100000 * 100 / house_price)
+    print(f"{100000 * 100 / house_price}%")
 else:
-    print(200000 * 100 / house_price)
+    print(f"{200000 * 100 / house_price}%")
