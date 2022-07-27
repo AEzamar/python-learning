b40e3981d64a386e343dabfa0f66eb87b20c8bfd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-27 06:50:48 -0500

    Added exercise4.py to re-learn basic conditional statements

---
+++ exercise4.py
@@ -0,0 +1,7 @@
+weather = input("How's the weather today? ")
+if(weather == 'hot'):
+    print("Drink plenty of water!")
+elif(weather == 'cold'):
+    print("Wear some warm clothes!")
+else:
+    print("It's a nice day!")
