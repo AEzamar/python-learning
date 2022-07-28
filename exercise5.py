b52e27131e9053862ee7c52237bf3fdeae483d1a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-28 06:46:00 -0500

    Another small change in sytanx to exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -1,6 +1,6 @@
 house_price = 1000000
 good_credit = False
 if(good_credit):
-    print(f"{house_price * 0.1}%")
+    print(f"${house_price * 0.1}")
 else:
-    print(f"{house_price * 0.2}%")
+    print(f"${house_price * 0.2}")
