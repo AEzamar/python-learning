61d927488e710c09028e4b18232a90aaca6417b4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-28 06:35:20 -0500

    Small changes to syntax in exercise5.py

--- exercise5.py
+++ exercise5.py
@@ -1,6 +1,6 @@
 house_price = 1000000
 good_credit = False
 if(good_credit):
-    print(f"{100000 * 100 / house_price}%")
+    print(f"{house_price * 0.1}%")
 else:
-    print(f"{200000 * 100 / house_price}%")
+    print(f"{house_price * 0.2}%")
