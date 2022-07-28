de771d684149c9bdba1528f9bedbaa69496a518a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-07-28 06:53:47 -0500

    Made simple script to practice condtional operators with exercise5.1.py

---
+++ exercise5.1.py
@@ -0,0 +1,6 @@
+high_income = True
+good_credit = True
+if(high_income and good_credit):
+    print("You're elegigle for a loan")
+else:
+    print("Sorry, we can't offer you a loan")
--- exercise5.py
+++ exercise5.py
@@ -1,5 +1,5 @@
 house_price = 1000000
-good_credit = False
+good_credit = True
 if(good_credit):
     print(f"${house_price * 0.1}")
 else:
