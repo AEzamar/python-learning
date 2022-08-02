6edca309941fce284b7c0cc7cf0e28616a909d0b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-02 06:24:03 -0500

    Testing guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,7 @@
 print("Guess the number between 1 and 10, you only have 3 opportunities!")
 se_number = 5
-guess_count = 0
-while guess_count < 3:
+guess_count = 1
+while guess_count <= 3:
     guess = int(input("Guess "))
     if guess == se_number:
         print("You got the number, you win!")
