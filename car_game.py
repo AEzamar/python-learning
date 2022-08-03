e77476039966cdd08b60dbf4bee8631cee3b65fa
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-03 07:06:06 -0500

    Still figuring out this exercise

--- car_game.py
+++ car_game.py
@@ -1,20 +1,21 @@
-user_input = input("> ")
 help_menu = '''
 start - to start the car
 stop - to stop the car
 quit - to exit
 '''
 car_started = False
+user_input = input("> ")
 while user_input.lower() != "quit":
     user_input = input("> ")
     if user_input.lower() == "help":
         print(help_menu)
     elif user_input.lower() == "start" and not car_started:
-        car_started = True
         print("The car is moving!")
-        if user_input.lower() == "start" and car_started == True:
+        car_started = True
+    elif user_input.lower() == "start" and car_started == True:
             print("The car is already moving")
     elif user_input.lower() == "stop" and car_started:
         print("The car has stopped")
-        if user_input.lower() == "stop" and car_started == False:
+        car_started = False
+    elif user_input.lower() == "stop" and car_started == False:
             print("The car has already stopped!")
