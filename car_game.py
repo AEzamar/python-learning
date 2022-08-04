cc008ceea7c5f2aa4b7754e3deb81dbec8f6ee1b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-04 06:37:21 -0500

    Polishing some details on car_game.py

--- car_game.py
+++ car_game.py
@@ -4,19 +4,18 @@ help_menu = '''
 [quit - to exit]
 '''
 car_started = False
-user_input = input("> ")
-while user_input.lower() != "quit":
-    if user_input.lower() == "help":
+user_input = ""
+while user_input != "quit":
+    user_input = input("> ").lower()
+    if user_input == "help":
         print(help_menu)
-    elif user_input.lower() == "start" and not car_started:
+    elif user_input == "start" and not car_started:
         print("The car is moving!")
         car_started = True
-    elif user_input.lower() == "start" and car_started == True:
+    elif user_input == "start" and car_started == True:
             print("The car is already moving")
-    elif user_input.lower() == "stop" and car_started:
+    elif user_input == "stop" and car_started:
         print("The car has stopped")
         car_started = False
-    elif user_input.lower() == "stop" and car_started == False:
+    elif user_input == "stop" and car_started == False:
             print("The car has already stopped!")
-    user_input = input("> ")
-
