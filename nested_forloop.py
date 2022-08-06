c51a3809f736a414638ab59436bf42111e5a19f9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-06 13:19:41 -0500

    Simple change and cleanup of nested_forloop.py

--- nested_forloop.py
+++ nested_forloop.py
@@ -1,7 +1,3 @@
-x = 0
-y = 0
-for xcoor in range(x, 5):
-    for ycoor in range(y, 5):
-        print(f"{x}, {y}")
-        x += 1
-        y += 1
+for xcoor in range(4):
+    for ycoor in range(3):
+        print(f"({x}, {y})")
