0fedff93cabad1aedc96139b3f08282067508a86
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-06 13:49:00 -0500

    Still doing this exercise

--- print_f.py
+++ print_f.py
@@ -1,5 +1,6 @@
 symbol = "x"
+str = ""
 numbers = [5, 2, 5, 2, 2]
 for num in numbers:
-    for str in range(1, num + 1):
-        print(f"{str}")
+    for n in range(1, num + 1):
+        print(f"{symbol}")
