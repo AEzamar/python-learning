30feae9bbadb7d3e7f4c79121b7ccc89faa5d691
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-07 08:47:38 -0500

    solved the challenge to print an f with a nested for loop

--- print_f.py
+++ print_f.py
@@ -1,6 +1,7 @@
 numbers = [5, 2, 5, 2, 2]
-for num in numbers:
+numbers1 = [1, 1, 1, 1, 5]
+for num in numbers1:
     str = ""
     for n in range(1, num + 1):
         str += "x"
-        print(str)
+    print(str)
