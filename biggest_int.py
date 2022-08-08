9e145ec4573bc1e89b22b7c67cc90e02249b4e53
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-08 06:36:10 -0500

    Did some small changes to the name of a variable and did some testing

--- biggest_int.py
+++ biggest_int.py
@@ -1,6 +1,6 @@
 numbers = [4, 20, 69, 69.5, 33, 44, 11, 12, 21]
-biggest_int = numbers[0]
+biggest = numbers[0]
 for num in numbers:
-    if num > biggest_int:
-        biggest_int = num
-print(biggest_int)
+    if num > biggest:
+        biggest = num
+print(biggest)
