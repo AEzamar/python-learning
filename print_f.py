d3c3b8c2121ad3e2f0af7182c92c54eef29237f5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-07 21:40:40 -0500

    Very small change to print_f.py

--- print_f.py
+++ print_f.py
@@ -2,6 +2,6 @@ numbers = [5, 2, 5, 2, 2]
 numbers1 = [1, 1, 1, 1, 5]
 for num in numbers1:
     str = ""
-    for n in range(1, num + 1):
+    for n in range(num + 1):
         str += "x"
     print(str)
