5a692c38bcacfa65d5a149133152426ca87f8b07
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-08 06:25:48 -0500

    First iteration  of solution to challenge for finding the biggest integer

--- biggest_int.py
+++ biggest_int.py
@@ -1,1 +1,8 @@
 numbers = [4, 20, 69, 55, 33, 44, 11, 12, 21]
+biggest_int = numbers[0]
+for num in numbers:
+    if num > biggest_int:
+        bigeest_int = num
+    else:
+        print(num)
+    print(biggest_int)
