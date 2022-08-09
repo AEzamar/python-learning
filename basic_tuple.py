8fee84453a914098e2db67605ff5436476024247
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-09 07:24:37 -0500

    Learning about tuples and unpacking in basic_tuple.py

--- basic_tuple.py
+++ basic_tuple.py
@@ -1,3 +1,9 @@
 tuple_1 = (1, 2, 3, 4)
 print(tuple_1)
-tuple_1[0] = 10
+# tuple_1[0] = 10
+coordinates = (10, 20, 30)
+#This is known as unpacking in python
+(x, y, z) = coordinates
+print(x)
+print(y)
+print(z)
