f19c5fe7c9f3c392c98530947c2fbfe771e41724
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-09 06:39:09 -0500

    Created new python script to solve challenge of finding and removing duplicate elements from a list

---
+++ remove_duplicates.py
@@ -0,0 +1,9 @@
+from codecs import unicode_escape_decode
+
+
+numbers = [2, 3, 1, 3, 4, 2, 3, 6, 7, 5, 4, 8, 8, 9]
+unique_list = []
+for number in numbers:
+    if number not in unique_list:
+        unique_list.append(number)
+print(unique_list)
