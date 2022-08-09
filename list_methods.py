f888e333fd15a739295696b0a99f4831f6b029ed
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-09 06:24:21 -0500

    Still exploring list methods in list_methods.py

--- list_methods.py
+++ list_methods.py
@@ -1,6 +1,8 @@
 numbers = [2, 5, 1, 3, 9, 69, 4, 7, 8, 10]
+numbers2 = numbers.copy()
 numbers.append(22)
 numbers.insert(6, 33)
 numbers.remove(69)
 numbers.sort()
 print(numbers)
+print(f"Numbers copy: {numbers2}")
