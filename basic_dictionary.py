c543ccd253c638983d9b0b4e14958f98ff436807
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-10 06:52:25 -0500

    Experimenting with dictionaries in python

--- basic_dictionary.py
+++ basic_dictionary.py
@@ -5,5 +5,9 @@ user = {
 }
 name = user["name"]
 print(name)
+#If we try to access an unexistent key with .get() we onyl get none
+#instead of crashing the whole script
+print(user.get("birthdate"))
+#print(user.get("birthdate", "Feb 14 1988")) #This way we can define a key/value pair if it doesn't exist
 for key in user:
     print(user[key])
