074952aa03a58eea8148f0168410beb83181f83d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-11 07:34:46 -0500

    Minor changes to str_to_emoji.py

--- str_to_emoji.py
+++ str_to_emoji.py
@@ -7,8 +7,9 @@ emoji_dty = {
 feeling = input("How are you feeling today? ")
 emoji_str = ""
 for str_emoji in emoji_dty:
-    print(str_emoji)
+    #print(str_emoji)
     for char in feeling:
         if char == str_emoji:
+            print(feeling.replace(char, emoji_dty[char]))
             emoji_str += feeling.replace(char, emoji_dty[str_emoji])
 print(emoji_str)
