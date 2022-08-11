2de1910f5bbef2f5a6dc093fbda56797b4094397
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-11 07:32:55 -0500

    Trying to figure out this exercise

--- str_to_emoji.py
+++ str_to_emoji.py
@@ -6,5 +6,9 @@ emoji_dty = {
 }
 feeling = input("How are you feeling today? ")
 emoji_str = ""
-for char in feeling:
-    emoji_str += feeling.replace(char == emoji_dty[char], emoji_dty[char])
+for str_emoji in emoji_dty:
+    print(str_emoji)
+    for char in feeling:
+        if char == str_emoji:
+            emoji_str += feeling.replace(char, emoji_dty[str_emoji])
+print(emoji_str)
