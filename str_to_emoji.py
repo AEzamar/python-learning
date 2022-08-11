2e1a9be5a63ddb52b0b5c9f12b013ad94d3ebc47
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-11 09:51:49 -0500

    Exercise finished, going to refactor and clea up the code a bit

--- str_to_emoji.py
+++ str_to_emoji.py
@@ -4,10 +4,14 @@ emoji_dty = {
     ":D": "😃",
     "T_T": "😢"
 }
+emoji = ""
+emoji_str = " "
 feeling = input("How are you feeling today? ")
 char_list = feeling.split()
 for key in emoji_dty:
     for word in char_list:
         if word == key:
             emoji = emoji_dty[key]
+            emoji_str += feeling.replace(word, emoji)
+print(emoji_str)
             
