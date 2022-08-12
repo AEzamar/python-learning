d4949c46cff0d80c810a9cf563a03a86644f12d5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-12 06:53:16 -0500

    Wrapped the code of the exercise in str_to_emoji.py in a function

--- str_to_emoji.py
+++ str_to_emoji.py
@@ -1,12 +1,16 @@
-feeling = input("How are you feeling today? ")
-emoji_dty = {
-    ":)": "😺",
-    ":(": "☹️",
-    ":D": "😃",
-    "T_T": "😢"
-}
-emoji_str = " "
-word_list = feeling.split(' ')
-for word in word_list:
-    emoji_str += emoji_dty.get(word, word) + " "
-print(emoji_str)
+def emoji_converter():
+    feeling = input("How are you feeling today? ")
+    emoji_dty = {
+        ":)": "😺",
+        ":(": "☹️",
+        ":D": "😃",
+        "T_T": "😢"
+    }
+    emoji_str = " "
+    word_list = feeling.split(' ')
+    for word in word_list:
+        emoji_str += emoji_dty.get(word, word) + " "
+    print(emoji_str)
+
+
+emoji_converter()
