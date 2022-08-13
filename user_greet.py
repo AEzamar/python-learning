dfa7c180e314d8b9088e00ae7d93ea45f16af399
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-13 09:05:37 -0500

    Added simple comment for keyword arguments

--- user_greet.py
+++ user_greet.py
@@ -3,5 +3,6 @@ def greet_user(f_name, l_name):
     print("Welcome aboard")
 
 
+#Keyword arguments
 greet_user(l_name="Smith", f_name="John") #This is another way to do positional arguments and it's called keyword arguments were instead of relying on position you tell python what argument belongs to what parameter
 greet_user("Mary", "Schmary") 
