129f7b8b52e5adeb5ecd00525b61623d5fcd9478
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-14 09:43:09 -0500

    Added a new method called break inside Vehicle class

--- basic_classes.py
+++ basic_classes.py
@@ -5,8 +5,13 @@ class Vehicle:
         self.speed = speed
     def accelerate(self):
         self.speed += 10
-        return f"Vehicle is now going at {self.speed} km/h"
-
+        return f"Vehicle is now going at {self.speed} km/h!"
+    def brake(self):
+        self.speed -= 5
+        return f"Vehicle slowed down to {self.speed} km/h!"
 
 truck = Vehicle("Mercedes Benz", "TZ100", 70)
 print(truck.accelerate())
+print(truck.accelerate())
+print(truck.accelerate())
+print(truck.brake())
