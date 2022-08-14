7b385cf3570c3a4c2773e02f9a843612bb7c3185
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-14 12:05:33 -0500

    Simple exercise to create a class called Person

---
+++ person_class.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def talk(self):
+        return f"Hello, my name is {self.name} and I'm {self.age} years old!"
+
+
+ed = Person("Eduardo", 34)
+print(ed.talk())
