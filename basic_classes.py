548db299cc2f9f4551eb46c5ff98505ab3f5aeb1
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-14 09:32:06 -0500

    Re-learning classes again, I remember the general idea but keep forgetting specific syntax

---
+++ basic_classes.py
@@ -0,0 +1,12 @@
+class Vehicle:
+    def __init__(self, make, model, speed):
+        self.make = make
+        self.model = model
+        self.speed = speed
+    def accelerate(self):
+        self.speed += 10
+        print(f"Vehicle is now going at {self.speed} km/h")
+
+
+truck = Vehicle("Mercedes Benz", "TZ100", 70)
+print(truck.accelerate())
