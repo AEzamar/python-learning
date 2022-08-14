b17eb0aacd719f55358a0f73effd5cad8ffa8207
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-14 09:06:51 -0500

    Learning basic of exceptions in python

--- basic_exceptions.py
+++ basic_exceptions.py
@@ -1,5 +1,9 @@
 try:
     age = int(input("Age: "))
+    income = 20000
+    risk = income /age
     print(age)
+except ZeroDivisionError:
+    print("The input for age cannot be zero!")
 except ValueError:
     print("Invalid value, please enter a number!")
