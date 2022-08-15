23d96ff6e07ca90567f8d1a6f1d75e0013d9dae1
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-15 06:44:57 -0500

    Added new parent class called Mammal and made a new sub-class name Dog

--- class_inheritance.py
+++ class_inheritance.py
@@ -1,13 +1,17 @@
-class Dog:
+class Mammal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
     def info(self):
         return f"{self.name} is {self.age} years old!"
+    
+
+class Dog(Mammal):
     def bark(self):
         return f"{self.name} says: Woof Woof"
 
-class Cat(Dog):
+        
+class Cat(Mammal):
     def meow(self):
         return f"{self.name} says: meow meow!"
 
