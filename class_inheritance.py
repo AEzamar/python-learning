f5b734856847f4421d3b57e26564e0299ee49741
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-15 06:19:14 -0500

    Added a simple method to the Dog class

--- class_inheritance.py
+++ class_inheritance.py
@@ -1,9 +1,11 @@
 class Dog:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+    def info(self):
+        return f"{self.name} is {self.age} years old!"
     def bark(self):
         return f"{self.name} says: Woof Woof"
 
 
-dugger = Dog("Dugger")
+dugger = Dog("Dugger", 7)
 print(dugger)
