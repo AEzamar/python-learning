ebfd3eb4a2ace808784b592932e8e752b6263722
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-16 06:37:32 -0500

    Turned a script to find the biggest number in a list to a funciton to be used as a module by app.py and imported the function in app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@
 #With the 2 lines below we import specific functions from the module and we can tell like would with regular functions
 from converters import lbs_to_kg
 from converters import kg_to_lbs
+from biggest_int import max_int
 print(lbs_to_kg(120))
 print(kg_to_lbs(67))
+numbers = [1, 6, 60, 31, 69, 77, 88, 2, 3, 5, 87]
+print(max_int(numbers))
--- biggest_int.py
+++ biggest_int.py
@@ -1,6 +1,6 @@
-numbers = [4, 20, 69, 69.5, 33, 44, 11, 12, 21]
-biggest = numbers[0]
-for num in numbers:
-    if num > biggest:
-        biggest = num
-print(biggest)
+def max_int(list):
+    biggest = list[0]
+    for num in list:
+        if num > biggest:
+            biggest = num
+    return biggest
