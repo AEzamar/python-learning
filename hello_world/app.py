6b37ef0e079701cdcb0929f888a0bf195936709b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-17 06:39:11 -0500

    Made some changes to app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,8 +6,10 @@ from converters import kg_to_lbs
 from utils import find_max
 #importing modules from a package
 import ecommerce.shipping
-ecommerce.shipping.calc_shipping()
+#ecommerce.shipping.calc_shipping()
+from ecommerce.shipping import calc_shipping
 print(lbs_to_kg(120))
 print(kg_to_lbs(67))
 numbers = [1, 6, 60, 31, 69, 77, 88, 2, 3, 5, 87]
 print(find_max(numbers))
+calc_shipping()
