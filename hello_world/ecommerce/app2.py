20fb593d6df1842f81c522723aa0354ff2137240
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-17 07:47:14 -0500

    Printed names from a list at random using random.choice in app2.py

--- hello_world/ecommerce/app2.py
+++ hello_world/ecommerce/app2.py
@@ -2,3 +2,8 @@ import random
 for i in range(3):
     print(random.random())
     print(random.randint(10, 20))
+
+
+members = ["John", "Mary", "Mosh", "Bob"]
+leader = random.choice(members)
+print(leader)
