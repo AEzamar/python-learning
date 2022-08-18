4635aa9cca16130b44ddead35a9253da0b9e8253
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-18 06:36:39 -0500

    Learned how to use a class without arguments, using the class name as if calling a function, storing it in variable and calling the method with the variable

--- dice_roll.py
+++ dice_roll.py
@@ -1,8 +1,9 @@
 import random
 class Dice:
-    def roll():
+    def roll(self):
         l_die = random.randint(1, 10)
         r_die = random.randint(1, 10)
         return l_die, r_die
 
-print(Dice.roll())
+dice = Dice()
+print(dice.roll())
