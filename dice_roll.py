fe644aed2744fcd0db91eb869aeaf32a4239d6f0
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-18 06:44:40 -0500

    Added an addtional space between module and import and class declaration

--- dice_roll.py
+++ dice_roll.py
@@ -1,9 +1,12 @@
 import random
+
+
 class Dice:
     def roll(self):
         l_die = random.randint(1, 10)
         r_die = random.randint(1, 10)
         return l_die, r_die
+
 
 dice = Dice()
 print(dice.roll())
