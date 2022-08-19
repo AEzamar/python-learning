5b71c9f17c68f9cab0cc6667c39484c5a8f61926
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-19 07:11:10 -0500

    Made small correction

--- excel_project/app.py
+++ excel_project/app.py
@@ -7,5 +7,5 @@ sheet = wb['Sheet1']
 cell = sheet.cell(1, 1)
 #print(cell.value)
 #max_row = sheet.max_row
-for row in range(1, sheet.max_row):
+for row in range(1, sheet.max_row + 1):
     print(row)
