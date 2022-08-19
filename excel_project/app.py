6a5b273958d4a92aa7f74e5dfb33cf7d90b10fb6
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-19 06:37:43 -0500

    Added a comment and started to work with the basics of openpyxl

--- excel_project/app.py
+++ excel_project/app.py
@@ -1,5 +1,6 @@
 import openpyxl as xl
 
 
-xl.load_workbook('transactions.xlsx')
-
+wb = xl.load_workbook('transactions.xlsx')
+#In wb['Sheet'] sheet is case sensitive an needs an uppercase letter at the start
+sheet = wb['Sheet1']
