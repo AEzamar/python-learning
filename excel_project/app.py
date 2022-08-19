b0c04568d0427ac40e01ba8f7a888e98d79e5819
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-19 07:15:09 -0500

    Working with sheets ad cells

--- excel_project/app.py
+++ excel_project/app.py
@@ -4,8 +4,9 @@ import openpyxl as xl
 wb = xl.load_workbook('transactions.xlsx')
 #In wb['Sheet'] sheet is case sensitive an needs an uppercase letter at the start
 sheet = wb['Sheet1']
-cell = sheet.cell(1, 1)
+#cell = sheet.cell(1, 1)
 #print(cell.value)
 #max_row = sheet.max_row
-for row in range(1, sheet.max_row + 1):
-    print(row)
+for row in range(2, sheet.max_row + 1):
+    cell = sheet.cell(row, 3)
+    print(cell)
