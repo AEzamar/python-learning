7502219fe4e5c4d061edf3b9ce80ebd13e24f688
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-19 07:09:00 -0500

    Learning basics of openpyxl

--- excel_project/app.py
+++ excel_project/app.py
@@ -6,4 +6,6 @@ wb = xl.load_workbook('transactions.xlsx')
 sheet = wb['Sheet1']
 cell = sheet.cell(1, 1)
 #print(cell.value)
-print(sheet.max_row)
+#max_row = sheet.max_row
+for row in range(1, sheet.max_row):
+    print(row)
