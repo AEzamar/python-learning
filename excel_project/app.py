d1151ecd492e60810157159647e7db9b99a30f34
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-19 06:49:23 -0500

    Still learning the basics, working with a simple spreadsheet

--- excel_project/app.py
+++ excel_project/app.py
@@ -4,3 +4,6 @@ import openpyxl as xl
 wb = xl.load_workbook('transactions.xlsx')
 #In wb['Sheet'] sheet is case sensitive an needs an uppercase letter at the start
 sheet = wb['Sheet1']
+cell = sheet.cell(1, 1)
+#print(cell.value)
+print(sheet.max_row)
