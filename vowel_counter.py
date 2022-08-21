180cc6a0008b132e551f592d56dbfb7e9fa74281
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-21 12:15:17 -0500

    Vowels are not being counted correctly, testing and refactoring

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,12 +1,14 @@
 def vowel_counter(str):
-    vowels = 'aeiou'
+    vowels_list = ['a', 'e', 'i', 'o', 'u']
     vowel_count = 0
     itn = 0
     for char in str:
-        if char == vowels[itn]:
+        if char == vowels_list[itn]:
             vowel_count += 1
+            print(char)
     itn += 1
     print(f"The number of vowels in this string is: {vowel_count}")
 
 
 vowel_counter("zdslw")
+vowel_counter("Lalafell")
