b2c750b4dee4ba8a2f347e2f114ec6f10f191f2e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 21:38:48 -0500

    Save changes to spreadsheet in copy of transactions.xlsx name transactions2.xlsx

--- excel_project/app.py
+++ excel_project/app.py
@@ -12,3 +12,6 @@ for row in range(2, sheet.max_row + 1):
     corrected_price = cell.value * 0.9
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
+
+
+wb.save('transactons2.xlsx')
