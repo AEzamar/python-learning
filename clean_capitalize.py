f96460d9d7b64431fad9c893dd20804e58705986
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 23:27:11 -0500

    Testing function in clean_capitalize.py

--- clean_capitalize.py
+++ clean_capitalize.py
@@ -5,3 +5,7 @@ def clean_capitalize(str_list):
         up_str = low_str[0].upper() + low_str[1:]
         clean_str_list.append(up_str)
     print(clean_str_list)
+
+
+word_list = ['HeLlO', 'dANg', "sHHieet", "CRAP", "manG", "MenG"]
+clean_capitalize(word_list)
