bb3c883a794affc18f3dc85b91cde2bef9f250e4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 22:14:32 -0500

    Organizing code into a reusable function to process many spreasheets dinamically

--- excel_project/app.py
+++ excel_project/app.py
@@ -2,26 +2,20 @@ import openpyxl as xl
 from openpyxl.chart import BarChart, Reference
 
 
-wb = xl.load_workbook('transactions.xlsx')
-#In wb['Sheet'] sheet is case sensitive an needs an uppercase letter at the start
-sheet = wb['Sheet1']
-#cell = sheet.cell(1, 1)
-#print(cell.value)
-#max_row = sheet.max_row
-for row in range(2, sheet.max_row + 1):
-    cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
-    corrected_price_cell = sheet.cell(row, 4)
-    corrected_price_cell.value = corrected_price
-
-
-values = Reference(sheet, 
-    min_row= 2, 
-    max_row = sheet.max_row,
-    min_col = 4,
-    max_col = 4)
-
-chart = BarChart()
-chart.add_data(values)
-sheet.add_chart(chart, 'e2')
-wb.save('transactons2.xlsx')
+def process_workbook(s_sheet):
+    wb = xl.load_workbook(f'{s_sheet}.xlsx')
+    sheet = wb['Sheet1']
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+    values = Reference(sheet, 
+        min_row= 2, 
+        max_row = sheet.max_row,
+        min_col = 4,
+        max_col = 4)
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+    wb.save('transactons2.xlsx')
