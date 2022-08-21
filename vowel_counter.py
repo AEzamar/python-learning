2b0252991de7e8d92125126eaae4c74d0ea28635
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-21 10:32:05 -0500

    Working on simple script to count vowels in a script

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,1 +1,9 @@
-v
+vowels = 'aeiou'
+itn = 0
+vowel_count = 0
+def vowel_counter(str):
+    for char in str:
+        if char == vowels[itn]:
+            vowel_count += 1
+    itn += 1
+    print(f"The number of vowels in this string is: {vowel_count}")
