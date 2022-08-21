d4924811b19ff5f9c84f5d094ffacd4d418a20e1
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 22:05:07 -0500

    Adding charts with openpyxl in transactions2.xlsx

--- excel_project/app.py
+++ excel_project/app.py
@@ -14,9 +14,14 @@ for row in range(2, sheet.max_row + 1):
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
 
-Reference(sheet, 
-    min_row=2, 
+
+values = Reference(sheet, 
+    min_row= 2, 
     max_row = sheet.max_row,
     min_col = 4,
     max_col = 4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 wb.save('transactons2.xlsx')
