89b582e35701b8c8ad1fc9ed0678730987e7d047
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 23:24:30 -0500

    Making a simple script to work with lists and strings in clean_capitalize.py

---
+++ clean_capitalize.py
@@ -0,0 +1,7 @@
+def clean_capitalize(str_list):
+    clean_str_list = []
+    for str in str_list:
+        low_str = str.lower()
+        up_str = low_str[0].upper() + low_str[1:]
+        clean_str_list.append(up_str)
+    print(clean_str_list)
