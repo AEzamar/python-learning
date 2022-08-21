659c9dbac414481e057925be8ca9146ba6375dd7
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-20 23:54:36 -0500

    Still refactoring

--- clean_capitalize.py
+++ clean_capitalize.py
@@ -1,8 +1,8 @@
 def clean_capitalize(str_list):
     clean_str_list = []
     for str in str_list:
-        low_str = str.lower() + str[0].upper() + str[1:]
-        clean_str_list.append(low_str)
+        low_str = str.lower()
+        clean_str_list.append(low_str[0].upper() + low_str[1:])
     print(clean_str_list)
 
 
