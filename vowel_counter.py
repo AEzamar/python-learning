66f8cce0d1d43410386fc37131612d1ee5a1ef27
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-21 21:42:57 -0500

    Made some changes and now it's working

--- vowel_counter.py
+++ vowel_counter.py
@@ -1,16 +1,12 @@
 def vowel_counter(str):
     vowels_list = ['a', 'e', 'i', 'o', 'u']
     vowel_count = 0
-    itn = 0
     for char in str.lower():
         if char in vowels_list: 
             vowel_count += 1
-            #print(char)
-            #print(vowels_list[itn])
-    itn += 1
         
-    print(f"The number of vowels in this string is: {vowel_count}")
+    return f"The number of vowels in this string is: {vowel_count}"
 
 
-vowel_counter("zdslw")
-vowel_counter("Lalafell")
+print(vowel_counter("zdslw"))
+print(vowel_counter("Lalafell"))
