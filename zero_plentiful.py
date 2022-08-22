ff2bcb018576407e9df980ba310b94913760c64b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-22 06:53:02 -0500

    Starting over

--- zero_plentiful.py
+++ zero_plentiful.py
@@ -4,20 +4,7 @@ def zero_plentiful(list):
     no_change_seq = 0
     for num in list:
         if num == 0:
-            zero_count += 1
-            if zero_count == 4:
-                no_change_seq += 1
-                sequence_count += 1
-            elif zero_count < 4:
-                zero_count = 0
-                sequence_count = 0
-        else:
-            zero_count = 0
-
-    if no_change_seq > 0 and sequence_count < 1:
-        return 0
-    elif no_change_seq > 0 and sequence_count > 0:
-        return no_change_seq
+            
 
 
 print(zero_plentiful([1, 2, 3, 0, 0, 0, 1, 0, 2]))
