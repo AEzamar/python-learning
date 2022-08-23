adb49283fc7c0d8b1453bf7b5dd4b5f5eeb87565
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-23 09:33:03 -0500

    I remembered to how to check length

--- excel_project/str_len.py
+++ excel_project/str_len.py
@@ -1,5 +1,5 @@
 def one_forty_chars(message = input("Type your message here ")):
-    if message.len() > 140:
+    if len(message) > 140:
         print("Message can't be longer than 140 characters")
     else:
         print("Looks good")
