2e4cb96212ed4c483b07021dd09dbd122b114285
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-23 09:10:58 -0500

    Very simple exercise to check the length of a string

---
+++ excel_project/str_len.py
@@ -0,0 +1,6 @@
+def one_forty_chars(message = input("Type your message here ")):
+    if message.length() > 140:
+        print("Message can't be longer than 140 characters")
+    else:
+        print("Looks good")
+
