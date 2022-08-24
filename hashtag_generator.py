0232552dec63a657fc51474ccb9f0811b451852e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-23 21:23:11 -0500

    Made a small change to normalize the string and then splitting it

--- hashtag_generator.py
+++ hashtag_generator.py
@@ -1,5 +1,5 @@
 def hashtag_generator(str):
-    str_list = str.split()
+    str_list = str.lower().split()
     upper_str_list = []
     for stri in str_list:
         upper_str_list.append(stri[0].upper() + stri[1:])
@@ -7,3 +7,4 @@ def hashtag_generator(str):
     return f'#{"".join(upper_str_list)}'
 
 print(hashtag_generator("This is a string"))
+print(hashtag_generator("OOOH shit mang!"))
