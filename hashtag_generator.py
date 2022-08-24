7edd6d7a5ee514bcbbb9940de67ac869584b056a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-23 21:24:21 -0500

    Seems to be working now

--- hashtag_generator.py
+++ hashtag_generator.py
@@ -3,8 +3,8 @@ def hashtag_generator(str):
     upper_str_list = []
     for stri in str_list:
         upper_str_list.append(stri[0].upper() + stri[1:])
-        upper_str_list
     return f'#{"".join(upper_str_list)}'
 
 print(hashtag_generator("This is a string"))
 print(hashtag_generator("OOOH shit mang!"))
+print(hashtag_generator("Apartheid Clyde can suck on my brown dick and balls"))
