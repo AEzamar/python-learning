a2ccc0784adc0577afe4b613629ed06665f6ac73
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-24 07:00:39 -0500

    Still tryig to solve this

--- upper_checker.py
+++ upper_checker.py
@@ -1,9 +1,12 @@
 def is_uppercase(inp):
+    upper_count = 0
     for char in inp:
-        if char != char.upper():
-            return False
-        else:
+        if char == char.upper():
+            upper_count += 1
+        if upper_count == len(inp):
             return True
+        else:
+            return False
 
 
-print(is_uppercase("THIS IS A STRING MANg"))
+print(is_uppercase("THIS IS A STRING MANG"))
