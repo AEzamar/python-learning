96551f72cd10ce84db1d112dc796e8b593c65ed9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-24 07:41:03 -0500

    Working on different solution in upper__checker1

--- upper_checker.py
+++ upper_checker.py
@@ -14,7 +14,7 @@ print(is_uppercase("THIS IS A STRING MANG"))
 
 def upper_checker1(inp):
     for char in inp:
-        print(char != char.upper())
+        char != char.upper()
 
 
 print(upper_checker1("THIS IS A STRiNG"))
