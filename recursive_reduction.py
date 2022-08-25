a5f3c8349daf9618883821a842aacdf939847495
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-25 06:54:29 -0500

    This solution seems to be working, going to test with cases from challenge

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -1,6 +1,8 @@
 def digital_root(n):
+    total = 0
     n_list = str(n).split()
-    return n_list
-
+    for digit in n_list[0]:
+        total += int(digit)
+    return total
 
 print(digital_root(123456))
