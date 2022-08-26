dfeb889b32d7c1dfa66490ba417daed9f073c0ba
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-26 07:33:25 -0500

    Still figuring this one out

--- reverse_str.py
+++ reverse_str.py
@@ -1,12 +1,6 @@
 def solution(string):
-    reversed_str = ""
     i = -1
-    for char in string:
-        print(string[-1])
-    while i < len(string):
-        reversed_str += string[i]
-        i += 1
-    return reversed_str
+    print(string[i])
 
 
 print(solution("Hello"))
