1da3b7ae12ba0549413a5fd9eb333a4eefe86c3f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-28 22:12:26 -0500

    This challenge is complete

--- str_cleaner.py
+++ str_cleaner.py
@@ -4,3 +4,4 @@ def string_clean(s):
 
 
 print(string_clean("Ohh12 dang112 mang"))
+print(string_clean("412For It4523 May9434 Seem3123, Life92323 Is313 Nothing563, But1312 A31 Dream98 Within9842 A12 Dream633123"))
