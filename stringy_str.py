7e3fd02edfc15a7b7034f1a7367baed082d6e94b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-30 07:01:18 -0500

    Trying to refactor this into a ternary operator but the syntax is not quite right

--- stringy_str.py
+++ stringy_str.py
@@ -1,16 +1,18 @@
-def stringy(size):
-    stringy_str = ""
-    a = '1'
-    b = '0'
-    for i in range(size):
-        if i % 2 == 0:
-            stringy_str += a
-        elif i % 1 == 0:
-            stringy_str += b
-    return stringy_str
+    def stringy(size):
+        stringy_str = ""
+        a = '1'
+        b = '0'
+        for i in range(size):
+            if i % 2 == 0 stringy_str += a else += b
+        """ for i in range(size):
+            if i % 2 == 0:
+                stringy_str += a
+            elif i % 1 == 0:
+                stringy_str += b """
+        return stringy_str
 
 
-print(stringy(5))
-print(stringy(6))
-print(stringy(1))
-print(stringy(2))
+    print(stringy(5))
+    print(stringy(6))
+    print(stringy(1))
+    print(stringy(2))
