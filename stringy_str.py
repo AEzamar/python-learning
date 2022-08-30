57acbb7a03d748c54606c232ed9ccac4162fa689
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-30 06:28:06 -0500

    Cleaned the script and solved the challenge

--- stringy_str.py
+++ stringy_str.py
@@ -3,29 +3,14 @@ def stringy(size):
     a = '1'
     b = '0'
     for i in range(size):
-        #print('iteration', i)
         if i % 2 == 0:
-            #print('i % 2', i)
             stringy_str += a
         elif i % 1 == 0:
-            #print('i % 3', i)
             stringy_str += b
     return stringy_str
 
 
 print(stringy(5))
 print(stringy(6))
-
-
-def stringy1(size):
-    str_list = []
-    a = '1'
-    b = '0'
-    for i in range(size):
-        str_list.append(a)
-    for it in range(size):
-        str_list.append(b)
-    return "".join(str_list)
-
-
-#print(stringy1(4))
+print(stringy(1))
+print(stringy(2))
