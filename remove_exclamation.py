3118a0bf12568179b7b99316a9b8d437475ab996
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-31 07:13:03 -0500

    Cleaned up the script, deleted one iteration that wasn't working, added proper indentation and both solutions are working

--- remove_exclamation.py
+++ remove_exclamation.py
@@ -12,25 +12,15 @@ print(remove('Hi!!'))
 print(remove('Hi!'))
 print(remove('!Hi! !Hi!'))
 
+
 def remove1(s):
-    for char in s:
-        print(len(s))
-        if s.index(char) == len(s) and char == '!':
-            print('Char index', s.index(char))
-            s.replace(char, "")
-    return s
-
-#print(remove1("Exclamation!!!"))
-
-
-def remove2(s):
     if s[-1] == '!':
         return s[:-1]
     else: 
         return s
 
 
-""" print(remove2('Exclamation!!!'))
-print(remove2('!Hi! Hi!'))
-print(remove2('Hi'))
-print(remove2('!Hi')) """
+""" print(remove1('Exclamation!!!'))
+print(remove1('!Hi! Hi!'))
+print(remove1('Hi'))
+print(remove1('!Hi')) """
