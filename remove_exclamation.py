2dae7db2e3dbb7ccdaba91f2b0c4b36e14dfdb53
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-08-31 06:21:25 -0500

    Still figuring this exercise out

--- remove_exclamation.py
+++ remove_exclamation.py
@@ -3,11 +3,15 @@ def remove(s):
     return re.sub('!', "", s)
 
 
-print(remove("Dang!!!!!"))
+#print(remove("Dang!!!!!"))
 
 
 def remove1(s):
     for char in s:
+        print(len(s))
         if s.index(char) == len(s) and char == '!':
+            print('Char index', s.index(char))
             s.replace(char, "")
     return s
+
+print(remove1("Exclamation!!!"))
