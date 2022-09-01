64f52ee885d306cd2db8411857b8ffce0545485c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-01 06:51:52 -0500

    Still working out the syntax in remove1

--- remove_end_exclamation.py
+++ remove_end_exclamation.py
@@ -5,6 +5,11 @@ def remove(s):
         if s[i] == '!':
             s.replace(s[i], "")
             i += -1
+        elif s[i] != '!':
+            return s
 
 print(remove("!!!Hi!!!"))
 print(-1 + -1)
+
+def remove1(s):
+    return s[:-1] if '!' in s 
