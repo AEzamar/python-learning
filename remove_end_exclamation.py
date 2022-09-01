33518398f5d527e6a214171b2e29cfab5500dbdc
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-01 06:48:24 -0500

    Trying different solutions for this challenge in remove1 function

--- remove_end_exclamation.py
+++ remove_end_exclamation.py
@@ -1,6 +1,10 @@
 import re
 def remove(s):
-    return re.sub('!$', "", s)
-
+    i = -1
+    for char in s:
+        if s[i] == '!':
+            s.replace(s[i], "")
+            i += -1
 
 print(remove("!!!Hi!!!"))
+print(-1 + -1)
