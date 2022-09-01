7b42be5d2fc1d65d47155a4896b165022e5e7756
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-01 06:59:14 -0500

    Still trying to solve this challenge

--- remove_end_exclamation.py
+++ remove_end_exclamation.py
@@ -3,13 +3,23 @@ def remove(s):
     i = -1
     for char in s:
         if s[i] == '!':
-            s.replace(s[i], "")
+            s.replace('!$', "")
             i += -1
         elif s[i] != '!':
             return s
 
+
 print(remove("!!!Hi!!!"))
 
+
+def remove1(s):
+    replaced_s = ""
+    if s[:-1] == '!':
+        replaced_s += s.replace('!', "")
+    return replaced_s
+
+
+print(remove1("!!!Hi!!!"))
 
 #def remove1(s):
     #return s[:-1] s.replace(s[-1], "") 
