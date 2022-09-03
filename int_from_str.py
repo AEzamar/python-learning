0cf87e985b2bb88a3fe4538b346102f56fc8551b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-03 00:32:33 -0500

    Made a very minor change to get_number_from_string.py

--- int_from_str.py
+++ int_from_str.py
@@ -5,7 +5,7 @@ def get_number_from_string(string):
     for char in string:
         if char in ints_str:
             ints += char
-    return ints
+    return int(ints)
 
 
 print(get_number_from_string("Hell1o Worl2d1"))
