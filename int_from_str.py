77ca9f470810f96413af985221801e2499750f18
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-02 23:59:32 -0500

    Challenge solved

--- int_from_str.py
+++ int_from_str.py
@@ -1,6 +1,11 @@
 import re
 def get_number_from_string(string):
-    return re.match('\d+', string)
-    
+    ints = ""
+    ints_str = '0123456789'
+    for char in string:
+        if char in ints_str:
+            ints += char
+    return ints
+
 
 print(get_number_from_string("Hell1o Worl2d1"))
