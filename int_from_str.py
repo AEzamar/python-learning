fcd8b59e563dec80d17f24308797503768b3a118
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-03 09:15:49 -0500

    Refactoring the function to extract numbers from a strin

--- int_from_str.py
+++ int_from_str.py
@@ -9,3 +9,9 @@ def get_number_from_string(string):
 
 
 print(get_number_from_string("Hell1o Worl2d1"))
+
+def get_number_from_string1(string):
+    return re.match('\d', string)
+
+
+print(get_number_from_string1("He2llo1 Wo1r1ld2"))
