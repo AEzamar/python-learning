fa594ba85b2f2e2cb030beb1ae7a2aa03cc0b42e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-05 06:57:15 -0500

    Tried starting fresh with another function

--- to_csv_text.py
+++ to_csv_text.py
@@ -6,14 +6,21 @@ def to_csv_text(list):
     return csv_text    
 
 
-print(to_csv_text([
+""" print(to_csv_text([
     [ 0, 1, 2, 3, 4 ],
     [ 10,11,12,13,14 ],
     [ 20,21,22,23,24 ],
-    [ 30,31,32,33,34 ]] ))
+    [ 30,31,32,33,34 ]] )) """
 
 
 def to_csv_text1(list):
     csv_text = ""
     for sublist in list:
-        print(sublist)
+        csv_text += str(','.join(sublist))
+
+
+print(to_csv_text1([
+    [ 0, 1, 2, 3, 4 ],
+    [ 10,11,12,13,14 ],
+    [ 20,21,22,23,24 ],
+    [ 30,31,32,33,34 ]]))
