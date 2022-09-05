43c361659f0ec2917419f229c25debae6c8e84fb
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-05 06:28:24 -0500

    Nested lists/arrays always give me trouble

--- to_csv_text.py
+++ to_csv_text.py
@@ -2,7 +2,7 @@ def to_csv_text(list):
     csv_text = ""
     i = 0
     for ele in list:
-        csv_text += str(ele[i])
+        csv_text += str(",".join(ele[i]))
         i += 1
     return csv_text    
 
