545c81928b0daa4d402c3aab397bc90f7e8db3a2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-06 06:24:55 -0500

    Made some small changes in to_csv_text.py

--- to_csv_text.py
+++ to_csv_text.py
@@ -6,11 +6,11 @@ def to_csv_text(list):
     return csv_text    
 
 
-""" print(to_csv_text([
+print(to_csv_text([
     [ 0, 1, 2, 3, 4 ],
     [ 10,11,12,13,14 ],
     [ 20,21,22,23,24 ],
-    [ 30,31,32,33,34 ]] )) """
+    [ 30,31,32,33,34 ]]))
 
 
 def to_csv_text1(list):
