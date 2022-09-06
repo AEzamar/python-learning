5ec1a5c07d1572ba1980d589aa642fdc0e569e28
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-06 10:06:22 -0500

    Seems like this is solved

--- digits_to_list.py
+++ digits_to_list.py
@@ -1,9 +1,7 @@
 def digitize(n):
-    n_list = str(n).split()
-    new_list = []
-    while len(n_list):
-        new_list.append(n_list.pop())
-    return new_list
+    n_list = list(str(n))
+    n_list.sort()
+    return n_list
 
 
 print(digitize(32514))
