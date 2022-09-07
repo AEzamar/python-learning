97a4245806294f568778a538db070c440bc53c5d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 06:36:41 -0500

    Challenge is solved with a traditional solution, going to refactor with list comprehension format later

--- mixed_sum.py
+++ mixed_sum.py
@@ -1,6 +1,8 @@
 def sum_mix(arr):
     total = 0
-    return [int(num) for num in arr]
+    for num in arr:
+        total += int(num)
+    return total
 
 
-print(sum_mix(['2', '3', '10', 4, 5, 6, 1, '12']))
+print(sum_mix(['2', '3', '10', 4, 5, 6, 1, '12', ]))
