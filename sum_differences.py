6f0b91424e8903db562c3e0204e6302676ece05d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 07:08:20 -0500

    Still working on sum_differences.py

--- sum_differences.py
+++ sum_differences.py
@@ -1,11 +1,10 @@
 def sum_of_differences(arr):
     result = 0
-    curr = 0
     next = 1
     arr.sort(reverse=True)
-    print(arr)
-    while len(arr):
-        result += arr[curr] - arr[next]
+    while len(arr) > 1:
+        result += arr[0] - arr[next]
+        print(result)
         arr.pop()
     return result
 
