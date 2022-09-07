ec7660d7e20c4d73481f58512789ed17c1cd7084
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 06:57:10 -0500

    Using while loop and pop list method to find and the differences

--- sum_differences.py
+++ sum_differences.py
@@ -2,13 +2,11 @@ def sum_of_differences(arr):
     result = 0
     curr = 0
     next = 1
-    descending = arr.sort(reverse=True)
-    print(descending)
-    """ while len(arr):
+    arr.sort(reverse=True)
+    print(arr)
+    while len(arr):
         result += arr[curr] - arr[next]
         arr.pop()
-        curr += 1
-        next += 1 """
     return result
 
 
