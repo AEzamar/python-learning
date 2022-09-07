66ca680816aac8f33bbd4d6fbf52777d9f66213e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 06:53:30 -0500

    Still figuring out how to solve this

--- sum_differences.py
+++ sum_differences.py
@@ -2,9 +2,14 @@ def sum_of_differences(arr):
     result = 0
     curr = 0
     next = 1
-    while len(arr):
+    descending = arr.sort(reverse=True)
+    print(descending)
+    """ while len(arr):
         result += arr[curr] - arr[next]
         arr.pop()
         curr += 1
-        next += 1
+        next += 1 """
     return result
+
+
+print(sum_of_differences([10, 2, 1]))
