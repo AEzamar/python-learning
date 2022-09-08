3d6cf6ab166423bbd6a32fe5e629d43e1aa06899
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-08 06:39:04 -0500

    Corrected small typo

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,4 +2,4 @@ def g_movies(lst):
     return [mov for mov in lst if mov.startswith('G')]
 
 
-print(g_movies(["Frankenstein", "Old Gals", "The Matrix", "Gatacca", "Gulliver", "Sausage Party"]))
+print(g_movies(["Frankenstein", "Old Gals", "The Matrix", "Gattaca", "Gulliver", "Sausage Party"]))
