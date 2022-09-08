8dfe35201a4199e0abfc7f6f513c55ab143b19f9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 19:58:10 -0500

    Trying to figure out how to take elements from two arrays at once if possible

--- merge_list.py
+++ merge_list.py
@@ -1,7 +1,10 @@
 def merge_arrays(first, second):
     merged_list = []
-    for num in first and second:
+    for num in (first, second):
         merged_list.append(num)
     merged_list.sort()
     return merged_list
+
+
+print(merge_arrays([3, 1, 1], [9, 6, 5]))
         
