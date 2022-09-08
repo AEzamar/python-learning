111e67690325833f45aeda823c0420fa5eb1723c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 19:47:31 -0500

    Solved the challenge and deleted an unnecessary variable

--- sum_differences.py
+++ sum_differences.py
@@ -1,12 +1,14 @@
 def sum_of_differences(arr):
     result = 0
-    next = 1
     arr.sort(reverse=True)
     while len(arr) > 1:
-        result += arr[0] - arr[next]
-        print(result)
+        result += arr[0] - arr[1]
         arr.remove(arr[0])
     return result
 
 
 print(sum_of_differences([10, 2, 1]))
+print(sum_of_differences([1, 1, 1, 1, 1]))
+print(sum_of_differences([-17, 17]))
+print(sum_of_differences([-1]))
+print(sum_of_differences([-3, -2, -1]))
