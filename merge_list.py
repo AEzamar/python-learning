2c449544045fd260c5f2707304116f10ca58d0de
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-07 20:25:02 -0500

    I forgot to make sure there's no duplicates in the list

--- merge_list.py
+++ merge_list.py
@@ -1,7 +1,8 @@
 def merge_arrays(first, second):
     merged_list = []
     for num in first + second:
-        merged_list.append(num)
+        if num not in merged_list:
+            merged_list.append(num)
     merged_list.sort()
     return merged_list
 
