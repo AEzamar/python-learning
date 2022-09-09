28f1cd78ad59c4f64188400c938f2aff7db69da9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-08 20:05:29 -0500

    Had to included ternary operator to account for tests cases that pass empty lists into the functio

--- find_list_avg.py
+++ find_list_avg.py
@@ -2,7 +2,8 @@ def find_average(nums):
     avg = 0
     for num in nums:
         avg += num
-    return avg / len(nums)
+    return 0 if nums == [] else avg / len(nums)
 
 
-print(find_average([9, 8, 9.5, 9.2, 9.1, 9.4, 9.1, 9.5]))
+print(find_average([9, 8, 9, 9, 9, 9, 9, 9]))
+print(find_average([]))
