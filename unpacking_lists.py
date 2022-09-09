4a1174f817925fed1938c48ceffd1a3980b84efc
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-09 11:39:53 -0500

    Added a comment about unpacking in a for loop declaration

--- unpacking_lists.py
+++ unpacking_lists.py
@@ -1,5 +1,6 @@
 letters = ["a", "b", "c"]
 #The enumerate method allows us to iterate and get then index of the items
 #on a list
+#It is also possible to unpack while declaring variables in a for loop
 for index, letter in enumerate(letters):
     print(f"{index}: {letter}")
