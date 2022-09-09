f960245b30a9bfdd3f530494c59aa12ff9127b20
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-09 11:38:20 -0500

    Learning more about list methods

---
+++ unpacking_lists.py
@@ -0,0 +1,5 @@
+letters = ["a", "b", "c"]
+#The enumerate method allows us to iterate and get then index of the items
+#on a list
+for index, letter in enumerate(letters):
+    print(f"{index}: {letter}")
