f93c8faf506078895079a52f2ae2bef955ea5083
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-11 10:07:06 -0500

    Working on recursive_reduction.py challenge

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -1,5 +1,10 @@
+import functools
+
+
 def digital_root(n):
     total = 0
+    root = functools.reduce(lambda accu, curr: accu + curr, [n])
+    print(root)
     n_list = str(n).split()
     for digit in n_list[0]:
         total += int(digit)
