9ae642065cc142da45864117bdf7f49f3e99083b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-11 11:28:59 -0500

    I can't reduce a single digit

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -3,9 +3,9 @@ import functools
 
 def digital_root(n):
     total = 0
+    n_list = str(n).split()
     root = functools.reduce(lambda accu, curr: accu + curr, [n])
     print(root)
-    n_list = str(n).split()
     """ for digit in n_list:
         n_list.map(int, digit) """
 
