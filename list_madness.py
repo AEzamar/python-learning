cbc057c5895fdba7d208693bf46a0e152a1973fd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-11 09:17:58 -0500

    This challenge is solved

--- list_madness.py
+++ list_madness.py
@@ -5,13 +5,6 @@ def array_madness(a, b):
     a_square = functools.reduce(lambda a, b: a + b, [num ** 2 for num in a])
     b_cube = functools.reduce(lambda a, b: a + b, [num ** 3 for num in b])
     return True if a_square > b_cube else False
-    """ for i in range(len(a_square)):
-        if a_square[i] > b_cube[i]:
-            return True
-        else:
-            return False """
-    """ print(a_square)
-    print(b_cube) """
 
 
 print(array_madness([4, 5, 6], [1, 2, 3]))
