8acba6ca47684d037a76a452a97c466270657994
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-11 11:37:03 -0500

    Had to go back to what I had earlier, function was getting too confusing

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -1,15 +1,9 @@
-import functools
-
-
 def digital_root(n):
     total = 0
     n_list = str(n).split()
-    root = functools.reduce(lambda accu, curr: accu + curr, [n])
-    print(root)
-    """ for digit in n_list:
-        n_list.map(int, digit) """
-
-    return root
+    for digit in n_list[0]:
+        total += int(digit)
+    return total
 
 
 print(digital_root(123456))
