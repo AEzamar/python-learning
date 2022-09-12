9ec441ef834e251422eed42c0744b3a832b356a2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-12 06:53:13 -0500

    Started and solved a challenge to find the smallest element in an array and either return that number or the  index of that number in the array

---
+++ excel_project/find_smallest.py
@@ -0,0 +1,9 @@
+def find_smallest(numbers, to_return):
+    if to_return.lower() == 'value':
+        return min(numbers)
+    elif to_return.lower() == 'index':
+        return numbers.index(min(numbers))
+
+
+print(find_smallest([4, 2, -1, 3, 5, 1], 'Value'))
+print(find_smallest([4, 2, -1, 3, 5, 1], 'Index'))
