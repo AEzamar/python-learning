eb381825927a3a551f23fb62fe7a3e58abcd36b2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-12 06:27:27 -0500

    I don't thin I can solve this yet

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -1,8 +1,8 @@
 def digital_root(n):
     total = 0
     n_list = list(str(n))
-    for digit in n_list[0]:
-        total += int(digit)
+    for digit in n_list:
+        total += sum(int(digit))
     return total
 
 
