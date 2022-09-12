1eb79bfa9b6e725ccfd40e5331396e545a40f04f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-12 06:38:59 -0500

    Eliminated some unused code in to_csv_text.py

--- to_csv_text.py
+++ to_csv_text.py
@@ -12,15 +12,5 @@ print(to_csv_text([
     [ 30,31,32,33,34 ]]))
 
 
-def to_csv_text1(list):
-    csv_text = ""
-    for sublist in list:
-        csv_text += "".join(sublist)
-    return csv_text
 
 
-""" print(to_csv_text1([
-    [ 0, 1, 2, 3, 4 ],
-    [ 10,11,12,13,14 ],
-    [ 20,21,22,23,24 ],
-    [ 30,31,32,33,34 ]])) """
