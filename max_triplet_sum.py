2674535a01f3da6cdbeee9115a32d421e0109bd3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-13 06:58:11 -0500

    Trying to solve max_triplet_sum.py

--- max_triplet_sum.py
+++ max_triplet_sum.py
@@ -1,8 +1,12 @@
 def max_tri_sum(numbers):
     no_dupp_numbers = []
-    for num in no_dupp_numbers:
+    for num in numbers:
         if num not in no_dupp_numbers:
             no_dupp_numbers.append(num)
+    print(no_dupp_numbers)
     no_dupp_numbers.sort(reverse=True)
     n1, n2, n3 = no_dupp_numbers
-    return
+    return n1 + n2 + n3
+
+
+print(max_tri_sum([3,2,6,8,2,3]))
