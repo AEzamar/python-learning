1b248e08090bb44c0d7c36ff9ac3ccfffe1adc98
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-13 07:15:40 -0500

    Trying to unpack and sum the first 3 elements of the uq_list list, don't know if that's possible

--- max_triplet_sum.py
+++ max_triplet_sum.py
@@ -5,7 +5,7 @@ def max_tri_sum(numbers):
             uq_list.append(num) """
     [uq_list.append(num) for num in numbers if num not in uq_list]
     uq_list.sort(reverse=True)
-    return uq_list[0] + uq_list[1] + uq_list[2]
+    return (n1 + n2 + n3) = uq_list
 
 
 print(max_tri_sum([3,2,6,8,2,3]))
