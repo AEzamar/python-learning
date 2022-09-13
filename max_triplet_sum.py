8a2b00db08588a34981d6b06705f90f74f299064
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-13 06:51:26 -0500

    Had to reverse list order in max_tri_sum function

--- max_triplet_sum.py
+++ max_triplet_sum.py
@@ -3,5 +3,6 @@ def max_tri_sum(numbers):
     for num in no_dupp_numbers:
         if num not in no_dupp_numbers:
             no_dupp_numbers.append(num)
+    no_dupp_numbers.sort(reverse=True)
     n1, n2, n3 = no_dupp_numbers
     return
