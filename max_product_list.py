f28ae8a0b3d98c1af053b2168a94955e234eb579
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 09:24:50 -0500

    Still working on this challenge

--- max_product_list.py
+++ max_product_list.py
@@ -1,6 +1,8 @@
+from audioop import reverse
 import functools
 def max_product(lst, n_largest_elements):
     sub_lst = lst[0 : n_largest_elements]
+    sub_lst.sort(reverse=True)
     return functools.reduce(lambda a, b: a * b, sub_lst)
 
 
