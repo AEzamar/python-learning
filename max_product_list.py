74c887f0e9e111dc51442e3b458016e9f5840e6d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 08:39:38 -0500

    Solving challenge in max_product_list.py

--- max_product_list.py
+++ max_product_list.py
@@ -1,2 +1,4 @@
+import functools
 def max_product(lst, n_largest_elements):
-    return n_largest_elements
+    sub_lst = lst[0 : n_largest_elements]
+    functools.reduce(lambda a, b: a * b, lst[range(n_largest_elements)])
