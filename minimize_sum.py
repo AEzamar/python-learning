e175a9597d5ddcb63fe6b1bed1fff52537da374b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 06:41:26 -0500

    Looking for ways to solve this

--- minimize_sum.py
+++ minimize_sum.py
@@ -1,6 +1,11 @@
 def min_sum(arr):
     total = 0
     arr.sort(reverse=True)
-    negative_i = -1
+    minus_i = -1
     for i in range(len(arr)):
-        total += arr[i] *
+        total += arr[i] * arr[minus_i]
+        minus_i += -1
+    return total
+
+
+print(min_sum([12, 6, 10, 26, 3, 24]))
