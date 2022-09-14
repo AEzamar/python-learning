3cd9c1c9bf92bad9fbac085edcf86e13752f81d4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 07:05:28 -0500

    Added user submitted solution, this coder used the sum method, multiplied the first and the last element in the list and in every iteration the loop gets shorter by two with range(len(arr)//2)

--- minimize_sum.py
+++ minimize_sum.py
@@ -15,3 +15,8 @@ def min_sum(arr):
 print(min_sum([12, 6, 10, 26, 3, 24]))
 print(min_sum([5,4,2,3]))
 print(min_sum([9,2,8,7,5,4,0,6]))
+
+#User submitted solution
+""" def min_sum(arr):
+    arr = sorted(arr)
+    return sum(arr[i]*arr[-i-1] for i in range(len(arr)//2)) """
