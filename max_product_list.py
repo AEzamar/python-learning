32b5a0989bd14833a3062f6c230f911c73ea52d6
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 09:34:33 -0500

    More small changes

--- max_product_list.py
+++ max_product_list.py
@@ -1,7 +1,7 @@
 import functools
 def max_product(lst, n_largest_elements):
     lst.sort(reverse=True)
-    sub_lst = lst[0:n_largest_elements]
+    sub_lst = lst[0 : n_largest_elements]
     return functools.reduce(lambda a, b: a * b, sub_lst)
 
 
