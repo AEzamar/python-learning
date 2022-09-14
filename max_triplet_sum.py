5fcaba52879546f6a7ce429622e1aec2c66c1e5c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-13 20:13:52 -0500

    Deleted some unnecessary code

--- max_triplet_sum.py
+++ max_triplet_sum.py
@@ -1,8 +1,5 @@
 def max_tri_sum(numbers):
     uq_list = []
-    """ for num in numbers:
-        if num not in uq_list:
-            uq_list.append(num) """
     [uq_list.append(num) for num in numbers if num not in uq_list]
     uq_list.sort(reverse=True)
     return uq_list[0] + uq_list[1] + uq_list[2]
