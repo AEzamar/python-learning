7d0822071772441d0f734da1cae965ef3d5ff7f4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-14 09:31:47 -0500

    Deleted an unused line

--- max_product_list.py
+++ max_product_list.py
@@ -1,8 +1,7 @@
 import functools
 def max_product(lst, n_largest_elements):
-    #lst.sort(reverse=True)
-    sub_lst = lst.sort(reverse=True)[:n_largest_elements]
-    print(sub_lst)
+    lst.sort(reverse=True)
+    sub_lst = lst[0:n_largest_elements]
     return functools.reduce(lambda a, b: a * b, sub_lst)
 
 
