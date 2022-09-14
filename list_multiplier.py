16bcb78a266386e18a9ee94a5b98de0cac14ccd5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-13 20:16:17 -0500

    The challenge in list_multiplier seems to be working

--- list_multiplier.py
+++ list_multiplier.py
@@ -2,6 +2,7 @@ def multiply_all(list):
     list_copy = list[:]
     def multiplier(int):
         return [num * int for num in list_copy]
+    return multiplier
 
 
-print(multiply_all([1, 2, 3]))
+print(multiply_all([1, 2, 3])(3))
