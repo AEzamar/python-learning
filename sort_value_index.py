a8d9a8bcf5f54b63a1eee4b30be90a39db51e39a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-15 10:57:42 -0500

    I almost had it with getting the square root of each number to return them in the desired order but the square root doesn't work correctly in all numbers like 23

--- sort_value_index.py
+++ sort_value_index.py
@@ -1,8 +1,14 @@
+import math
 def sort_by_value_and_index(lst):
     product_lst = [num * (i + 1) for i, num in enumerate(lst)]
+    print(product_lst)
     product_lst.sort()
-    return [num // (i + 1)  for i, num in enumerate(product_lst)]
+    return [math.sqrt(num)  for i, num in enumerate(product_lst)]
     #return product_lst
 
 
 print(sort_by_value_and_index([23, 2, 3, 4, 5]))
+
+""" print(math.sqrt(4))
+print(math.sqrt(9))
+print(math.sqrt(23)) """
