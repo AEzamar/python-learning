531678795e626c15608313574ed6fa5be116585d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-15 07:07:14 -0500

    This is taking more time than I thought

--- sort_value_index.py
+++ sort_value_index.py
@@ -4,7 +4,10 @@ def sort_by_value_and_index(lst):
     for ele in lst:
         product_lst.append((ele * i))
         i += 1
-    return product_lst
+        product_lst.sort()
+    return [num // i for num in product_lst]
+    #return product_lst
+
 
 
 print(sort_by_value_and_index([23, 2, 3, 4, 5]))    
