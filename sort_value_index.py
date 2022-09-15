2ddf539466036a1352b3da9d7674a023fac8766b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-15 06:53:38 -0500

    Still figuring this one out

--- sort_value_index.py
+++ sort_value_index.py
@@ -2,4 +2,9 @@ def sort_by_value_and_index(lst):
     i = 1
     product_lst = []
     for i in range(len(lst)):
-        product_lst.append(lst[i])
+        print(lst[i])
+        product_lst.append(lst[i] * i + 1)
+    return product_lst
+
+
+print(sort_by_value_and_index([23, 2, 3, 4, 5]))    
