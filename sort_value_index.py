22dee76c659e1961c0c889c1e9b62678f530f605
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-15 10:11:06 -0500

    This solution cannot solve this challenge, since the values of the list change position after sorting them, so dividing them yields unwanted numbers

--- sort_value_index.py
+++ sort_value_index.py
@@ -1,9 +1,8 @@
 def sort_by_value_and_index(lst):
     product_lst = [num * (i + 1) for i, num in enumerate(lst)]
     product_lst.sort()
-    return [(num // (i + 1))  for i, num in enumerate(product_lst)]
+    return [num // (i + 1)  for i, num in enumerate(product_lst)]
     #return product_lst
 
 
-
-print(sort_by_value_and_index([23, 2, 3, 4, 5]))    
+print(sort_by_value_and_index([23, 2, 3, 4, 5]))
