8d49bea7b359190dfb983cc73e8c0b678072dff0
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-15 23:23:43 -0500

    Going to finish this tomorrow

--- list_leader.py
+++ list_leader.py
@@ -1,5 +1,7 @@
 import functools
 def array_leader(lst):
+    leader = 0
     for num in lst:
         sub_arr = lst[num: -1]
-        if num > functools.reduce(sub_arr, lambda curr, next: curr + next) 
+        if num > functools.reduce(sub_arr, lambda curr, next: curr + next):
+            leader = num 
