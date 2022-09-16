cc7c4a07fb61bf43b30028c2a36aa7df714eeb03
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 08:26:11 -0500

    Still haven't solved this, seems I'm close but I keep getting numbers that should not be there

--- list_leader.py
+++ list_leader.py
@@ -7,13 +7,13 @@ def array_leader(numbers):
         print(sum(sub_arr)) """
         if num > sum(sub_arr):
             leaders.append(num)
-        if len(sub_arr) == 1 and sum(sub_arr) > 0:
+        elif len(sub_arr) == 1 and sum(sub_arr) > 0:
             leaders.append(sum(sub_arr))
     return leaders
 
 
 print(array_leader([1,2,3,4,0]))
-print(array_leader([1,2]))
+print(array_leader([-36,-12,-27]))
 
 
 def array_leader1(numbers):
