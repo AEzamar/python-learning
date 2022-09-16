16e551e5dbdbc4820a7b3e3bf50bdf84cee4e699
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 08:08:06 -0500

    I got a desired result, will do further testing

--- list_leader.py
+++ list_leader.py
@@ -2,8 +2,9 @@
 def array_leader(numbers):
     leader = 0
     for num in numbers:
-        sub_arr = numbers[numbers.index(num):]
+        sub_arr = numbers[num:]
         print(sub_arr)
+        print(sum(sub_arr))
         if num > sum(sub_arr):
             leader = num 
     return leader
