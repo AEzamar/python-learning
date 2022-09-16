ab7b707804bfb24f8c71eee86d2de3e15eb5a4ce
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 07:50:31 -0500

    Small changes to syntax

--- list_leader.py
+++ list_leader.py
@@ -1,10 +1,10 @@
 from functools import reduce
 def array_leader(numbers):
-    leader = numbers[0]
+    leader = '0'
     for num in numbers:
         sub_arr = numbers[num:-1]
-        if num > reduce(sub_arr, lambda curr, next: curr + next):
+        if num > reduce(lambda curr, next: curr + next, sub_arr):
             leader = numbers[num] 
-
+    return leader
 
 print(array_leader([1,2,3,4,0]))
