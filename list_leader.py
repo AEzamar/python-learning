974274cc3806a3968727fd8d6fb5a158424a4c99
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 08:02:25 -0500

    Still working on the challenge in list_leader.py

--- list_leader.py
+++ list_leader.py
@@ -2,9 +2,10 @@ from functools import reduce
 def array_leader(numbers):
     leader = '0'
     for num in numbers:
-        sub_arr = numbers[numbers.index(num):-1]
-        if num > reduce(lambda total, next: total + next, sub_arr):
-            leader = numbers[num] 
+        sub_arr = numbers[numbers.index(num):]
+        print(sub_arr)
+        """ if num > sum(sub_arr):
+            leader = numbers[num]  """
     return leader
 
 print(array_leader([1,2,3,4,0]))
