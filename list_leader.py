aee5ad868c466ae45b0f3a97e2b9f2bdde475ff4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 07:52:44 -0500

    Changed variable name in reduce lambda function from  curr to total

--- list_leader.py
+++ list_leader.py
@@ -2,8 +2,8 @@ from functools import reduce
 def array_leader(numbers):
     leader = '0'
     for num in numbers:
-        sub_arr = numbers[num:-1]
-        if num > reduce(lambda curr, next: curr + next, sub_arr):
+        sub_arr = numbers[numbers.index(num):-1]
+        if num > reduce(lambda total, next: total + next, sub_arr):
             leader = numbers[num] 
     return leader
 
