8a94eb1786dbf367e1526bad58a210cba4f726fe
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-16 08:11:46 -0500

    Trying to solve this in one line, but I still need to test my first solution

--- list_leader.py
+++ list_leader.py
@@ -3,10 +3,15 @@ def array_leader(numbers):
     leader = 0
     for num in numbers:
         sub_arr = numbers[num:]
-        print(sub_arr)
-        print(sum(sub_arr))
+        """ print(sub_arr)
+        print(sum(sub_arr)) """
         if num > sum(sub_arr):
             leader = num 
     return leader
 
+
 print(array_leader([1,2,3,4,0]))
+
+
+def array_leader1(numbers):
+    return [num for num in numbers if num > sum(numbers[num:])]
