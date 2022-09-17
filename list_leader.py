aa0dd665d8ed66b337910ca5691d105281c84c38
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-17 09:04:13 -0500

    Just making a clean version of array_leader3 function

--- list_leader.py
+++ list_leader.py
@@ -55,3 +55,22 @@ def array_leader3(numbers):
 print(array_leader3([1,2,3,4,0]))
 print(array_leader3([-36,-12,-27]))
 print(array_leader3([16, 17, 4, 3, 5, 2]))
+
+
+def array_leader4(numbers):
+    leaders = []
+    #print(numbers)
+    while len(numbers):
+        if numbers[0] > sum(numbers[1:]):
+            leaders.append(numbers[0])
+        del numbers[0]
+        #print('Leaders 1:', leaders)
+        """ if len(numbers) == 1 and sum(numbers) > 0:
+            leaders.append(sum(numbers))
+        print('Leaders 2:', leaders) """
+    return leaders
+
+
+print(array_leader4([1,2,3,4,0]))
+print(array_leader4([-36,-12,-27]))
+print(array_leader4([16, 17, 4, 3, 5, 2]))
