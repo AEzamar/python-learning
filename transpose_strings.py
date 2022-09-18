8084a22b140e16fa9304da885da7be75ab02e42d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-18 11:30:54 -0500

    Still working with the tuples

--- transpose_strings.py
+++ transpose_strings.py
@@ -2,8 +2,8 @@ def transpose_two_strings(arr):
     i = 0
     transposed = ""
     for char in zip(arr[0], arr[1]):
-        transposed += str(char)
-    return transposed
+        transposed += str(char) + '\n'
+    return transposed.rstrip()
 
 
 print(transpose_two_strings(['Hello', 'World']))
