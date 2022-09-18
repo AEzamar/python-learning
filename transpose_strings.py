f0c03ecb52983ad31dfab7c612a128773af6da2a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-18 08:41:41 -0500

    Forgot how to iterate through two things at the same time

--- transpose_strings.py
+++ transpose_strings.py
@@ -1,7 +1,7 @@
 def transpose_two_strings(arr):
     i = 0
     transposed = ""
-    for chr in arr[0], chaar in arr[1]:
+    for chr in arr[0] and chaar in arr[1]:
         transposed += f"{chr, chaar}"
     return transposed
 
