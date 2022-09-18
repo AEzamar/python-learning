4c393ffd1919333c8919c0c06356141c2d5ce341
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-18 08:36:58 -0500

    Started new challenge with transpose_strings.py

---
+++ transpose_strings.py
@@ -0,0 +1,9 @@
+def transpose_two_strings(arr):
+    i = 0
+    transposed = ""
+    for char in arr[0] and chr in arr[1]:
+        transposed += f"{char, chr}"
+    return transposed
+
+
+print(transpose_two_strings(['Hello', 'World']))
