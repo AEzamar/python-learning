6f1525a1ad203c1af2aa70f5cfe43a146902c47b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-19 13:23:08 -0500

    This syntax is not working

--- transpose_strings.py
+++ transpose_strings.py
@@ -5,7 +5,7 @@ def transpose_two_strings(arr):
     #len_diff = len(arr[0]) - len(arr[1])
     """ for i in range(len(arr[0])):
         transposed += f"{arr[0][i]} {arr[1][i]}\n"  """
-    [char for char in arr[0] and chaar for chaar in arr[1]]
+    [(transposed += char) for char in arr[0] and (transposed += chaar) for chaar in arr[1]]
     return transposed.rstrip()
 
 
