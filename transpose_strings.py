02fdc25bf043ba22965ed4dbdbd9c51019d42f9a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-19 06:41:26 -0500

    Trying something to solve the problem of having strings of different lengths

--- transpose_strings.py
+++ transpose_strings.py
@@ -2,7 +2,8 @@ def transpose_two_strings(arr):
     transposed = ""
     """ for char in zip(arr[0], arr[1]):
         transposed += f"{str(char)}" + '\n' """
-    for i in range(len(arr[0])):
+    len_diff = len(arr[0]) - len(arr[1])
+    for i in range(len(arr[0]) + len_diff):
         transposed += f"{arr[0][i]} {arr[1][i]}\n" 
     return transposed.rstrip()
 
