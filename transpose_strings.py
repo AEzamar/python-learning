bc7e558e42886ee32c731910e209c83cdbc82b5d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-19 06:18:46 -0500

    Got back to the solution that kind of works in transpose_strings.py but ouputs the strings in tuples

--- transpose_strings.py
+++ transpose_strings.py
@@ -1,8 +1,7 @@
 def transpose_two_strings(arr):
     transposed = ""
-    transposed += [(char, chaar) for char, chaar in (arr[0], arr[1])]
-    """ for char in arr[0]:
-        transposed += f"{char}\n" """
+    for char in zip(arr[0], arr[1]):
+        transposed += str(char) + '\n'
     return transposed.rstrip()
 
 
