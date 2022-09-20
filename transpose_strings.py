f31568c3e61857c3e114b9a63ff5e698fdd30133
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-20 13:54:16 -0500

    I'm out of ideas to solve transpose_strings.py

--- transpose_strings.py
+++ transpose_strings.py
@@ -1,20 +1,16 @@
 def transpose_two_strings(arr):
     transposed = ""
-    #len_diff = len(arr[0]) - len(arr[1])
-    """ for i in range(len(arr[0])):
-        transposed += f"{arr[0][i]} {arr[1][i]}\n"  """
-    #transposed += str([(char, chaar) for char in arr[0] for chaar in arr[1]])
-    """ for char in zip(arr[0], arr[1]): """
-    """     transposed += '\n' """
-    """     transposed += f"{str(char)}" """
-    main_len = 0
+    """ main_len = 0
     main_len += len(arr[0]) if len(arr[0]) > len(arr[1]) else len(arr[1])
-    #print(main_len)
     for i in range(main_len):
-        transposed += f"{arr[0][i]}, {arr[1][i]}"
+        transposed += '\n'
+        transposed += f"{arr[0][i]} {arr[1][i]}" """
+    for char in zip(arr[0], arr[1]):
+        transposed += '\n'
+        transposed += str(char)
     return transposed.lstrip()
 
 
 print(transpose_two_strings(['Hello', 'World']))
 print(transpose_two_strings(['Hey', 'People']))
-print(transpose_two_strings(['Tranpose', 'This']))
+#print(transpose_two_strings(['Tranpose', 'This']))
