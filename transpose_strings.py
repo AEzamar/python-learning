e1a066c42c898abb88f4055980056ae18784ea49
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-20 09:37:40 -0500

    The ternary operator to determine the length of the iteration is now working

--- transpose_strings.py
+++ transpose_strings.py
@@ -8,12 +8,13 @@ def transpose_two_strings(arr):
     """     transposed += '\n' """
     """     transposed += f"{str(char)}" """
     ran_len = 0
-    ran_len += len(arr[0]) if len(arr[0]) > len(arr[1]) else ran_len += len(arr[1])
-    print(ran_len)
-    for i in range(len(arr[0]) > len(arr[1]) or len(arr[1]) > len(arr[0])):
-        print(arr[0][i])
+    ran_len += len(arr[0]) if len(arr[0]) > len(arr[1]) else len(arr[1])
+    #print(ran_len)
+    """ for i in range(len(arr[0]) > len(arr[1]) or len(arr[1]) > len(arr[0])):
+        print(arr[0][i]) """
     return transposed.lstrip()
 
 
 print(transpose_two_strings(['Hello', 'World']))
-print(transpose_two_strings(["Hey", "People"]))
+print(transpose_two_strings(['Hey', 'People']))
+print(transpose_two_strings(['Tranpose', 'This']))
