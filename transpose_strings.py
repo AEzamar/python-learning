d9f18416447825a1b82ed6c8f25d8431ef7ffb92
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-20 06:08:35 -0500

    Still working on transpose_strings.py

--- transpose_strings.py
+++ transpose_strings.py
@@ -5,10 +5,11 @@ def transpose_two_strings(arr):
     #len_diff = len(arr[0]) - len(arr[1])
     """ for i in range(len(arr[0])):
         transposed += f"{arr[0][i]} {arr[1][i]}\n"  """
-    transposed += str([(char, chaar) for char in arr[0] for chaar in arr[1]])
+    #transposed += str([(char, chaar) for char in arr[0] for chaar in arr[1]])
     for char, chaar in zip(arr[0], arr[1]):
-        print(char, chaar)
-    return transposed.rstrip()
+        transposed += '\n'
+        transposed += f"{char} {chaar}"
+    return transposed.lstrip()
 
 
 print(transpose_two_strings(['Hello', 'World']))
