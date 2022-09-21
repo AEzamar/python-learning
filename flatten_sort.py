7836220e08c58a3ad1ccc9328533f6fa5f263b5d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-21 07:18:56 -0500

    This seems to be working, only need to sort now

--- flatten_sort.py
+++ flatten_sort.py
@@ -1,9 +1,7 @@
 def flatten_and_sort(array):
     flat_arr = []
-    i = 0
-    for j in range(len(array)):
-        flat_arr += f"{array[i][j]}"
-        i += 1
+    for i in range(len(array)):
+        flat_arr += array[i]
     return flat_arr
 
 print(flatten_and_sort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))
