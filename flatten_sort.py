d21891730515d9316a3aafd7a9981340a8bae832
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-21 07:20:49 -0500

    The function is returning the desired result, going to refactor this to make it quicker if possible

--- flatten_sort.py
+++ flatten_sort.py
@@ -2,6 +2,7 @@ def flatten_and_sort(array):
     flat_arr = []
     for i in range(len(array)):
         flat_arr += array[i]
+    flat_arr.sort()
     return flat_arr
 
 print(flatten_and_sort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]))
