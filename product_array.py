c8eb10898d99fe24c895335560ef2252413c6b21
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 06:57:09 -0500

    Added another user submitted solution, because it was so good

--- product_array.py
+++ product_array.py
@@ -11,3 +11,18 @@ def product_array(numbers):
 print(product_array([1, 2, 5]))
 print(product_array([3, 27, 4, 2]))
 print(product_array([13,10,5,2,9]))
+
+
+#User submitted solution
+""" from operator import mul
+from functools import reduce
+
+def product_array(numbers):
+    tot = reduce(mul,numbers)
+    return [tot//n for n in numbers] """
+
+#This solution is even better holy molly
+""" from numpy import prod
+def product_array(numbers):
+    p = prod(numbers)
+    return [p // i for i in numbers] """
