5b45f1d8a29e01fb2068f1d05ee7dfb0e0a9935e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 09:53:12 -0500

    The max_gap function seems to be working

--- max_gap.py
+++ max_gap.py
@@ -1,14 +1,10 @@
 def max_gap(numbers):
     diff_lst = []
     numbers.sort(reverse=True)
-    next = 1
-    print(numbers)
+    diff = numbers[0] - numbers[1]
     while len(numbers):
-        diff_lst.append(numbers[0] - numbers[1])
+        diff_lst.append(diff)
         del numbers[0]
-    """ for i in range(len(numbers) + 1):
-        diff_lst.append(numbers[i] - numbers[next])
-        next += 1 """
     return max(diff_lst)
 
 
