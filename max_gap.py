755103e0781b0725d9302bb0dd300ab2be9b616b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 07:21:39 -0500

    I think I might have to use a while loop and take out the first element of the list so I don't get out of range errors

--- max_gap.py
+++ max_gap.py
@@ -3,10 +3,10 @@ def max_gap(numbers):
     numbers.sort(reverse=True)
     next = 1
     print(numbers)
-    for i in range(len(numbers)):
+    """ for i in range(len(numbers) + 1):
         diff_lst.append(numbers[i] - numbers[next])
-        next += 1
-    return max(diff_lst)
+        next += 1 """
+    return diff_lst
 
 
 print(max_gap([13, 3, 5]))
