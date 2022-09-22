6e236857e0778e6c684e3e542700504527e39420
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 07:09:58 -0500

    Having trouble with the indexing

--- max_gap.py
+++ max_gap.py
@@ -1,4 +1,9 @@
 def max_gap(numbers):
     diff_lst = []
     numbers.sort(reverse=True)
-    
+    for i in range(len(numbers)):
+        diff_lst.append(numbers[i] - numbers[i + 1])
+    return max(diff_lst)
+
+
+print(max_gap([13, 3, 5]))
