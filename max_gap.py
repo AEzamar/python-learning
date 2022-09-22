5b96e2c3d7e304071769564cf51f6dacc7c8b92f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 10:04:19 -0500

    This seems to be working again, but going to use more tests cases with negative numbers to see if I get the expected result

--- max_gap.py
+++ max_gap.py
@@ -2,11 +2,17 @@ def max_gap(numbers):
     numbers_copy = numbers[:]
     diff_lst = []
     numbers_copy.sort(reverse=True)
-    diff = numbers_copy[0] - numbers_copy[1]
     while len(numbers_copy):
-        diff_lst.append(diff)
+        #diff = numbers_copy[0] - numbers_copy[1]
+        #print(numbers_copy)
+        diff_lst.append(numbers_copy[0] - numbers_copy[1])
         del numbers_copy[0]
+        if len(numbers_copy) == 1:
+            break
+    """ print(numbers_copy)
+    print(diff_lst) """
     return max(diff_lst)
 
 
 print(max_gap([13, 3, 5]))
+print(max_gap([13,10,2,9,5]))
