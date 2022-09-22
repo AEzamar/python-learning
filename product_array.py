e6c8cbc668bf9232d6a5348db8d55c9fa37a3f95
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-22 06:41:26 -0500

    I think this is working, going to do more testing

--- product_array.py
+++ product_array.py
@@ -1,8 +1,13 @@
+from functools import reduce
 def product_array(numbers):
     product_arr = []
     exclusion = 0
     for i in range(len(numbers)):
         copy_arr = numbers[:]
-        del numbers[exclusion]
-                    
+        del copy_arr[exclusion]
+        product_arr.append(reduce(lambda total, next: total * next, copy_arr))
+        exclusion += 1
     return product_arr
+
+
+print(product_array([1, 2, 5]))
