c48825a555be43de80c7918eb6bcb7410e2b9165
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 17:45:29 -0500

    Minor changes in syntax in nice_list.py

--- nice_list.py
+++ nice_list.py
@@ -1,11 +1,11 @@
 def is_nice(arr):
     nice_count = 0
     for num in arr:
-        if (num + 1) in arr:
+        if num + 1 in arr:
             nice_count += 1
-        elif(num - 1) in arr:
+        elif num - 1 in arr:
             nice_count += 1
-    return True if nice_count == len(arr) - 1 else False
+    return True if nice_count == len(arr) else False
 
 
 print(is_nice([2, 10, 9, 3]))
