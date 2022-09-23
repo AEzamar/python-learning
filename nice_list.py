c13f3ef9b3c73200f1dbf82207ecf8afd4d7b213
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 17:10:47 -0500

    This challenge is almost done but a few tests are not passing, refactoring noww

--- nice_list.py
+++ nice_list.py
@@ -1,7 +1,9 @@
 def is_nice(arr):
     nice_count = 0
     for num in arr:
-        if num + 1 or num - 1 in arr:
+        if (num + 1) in arr:
+            nice_count += 1
+        elif(num - 1) in arr:
             nice_count += 1
     return True if nice_count == len(arr) else False
 
