8819e4f2405fc624b9133df9be94af3fb6fc835e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 15:11:46 -0500

    Just trying a quick first approach to this challenge

--- nice_list.py
+++ nice_list.py
@@ -1,2 +1,6 @@
 def is_nice(arr):
-    
+    nice_count = 0
+    for num in arr:
+        if num + 1 or num - 1 in arr:
+            nice_count += 1
+    return True if nice_count == len(arr) else False
