203421df85b484685d5fc40e81ba4e6a4923936f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 18:56:34 -0500

    Trying to finish this

--- nice_list.py
+++ nice_list.py
@@ -1,11 +1,12 @@
 def is_nice(arr):
     nice_count = 0
+    nice_bool = False
     for num in arr:
         if num + 1 in arr:
             nice_count += 1
         elif num - 1 in arr:
             nice_count += 1
-    return True if nice_count == len(arr) else False
+    return nice_bool = True if nice_count == len(arr) else nice_bool = False
 
 
 print(is_nice([2, 10, 9, 3]))
