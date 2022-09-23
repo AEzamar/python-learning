3c1670e37ba0d109a317037b6ccd35851080c89b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 15:14:47 -0500

    Current solution not working

--- nice_list.py
+++ nice_list.py
@@ -4,3 +4,7 @@ def is_nice(arr):
         if num + 1 or num - 1 in arr:
             nice_count += 1
     return True if nice_count == len(arr) else False
+
+
+print(is_nice([2, 10, 9, 3]))
+print(is_nice([4, 2, 1]))
