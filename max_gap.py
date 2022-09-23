eb4db6fdf7bc3a127807caa0d1140b66cf5dc341
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-23 14:55:59 -0500

    Added some user submitted solutions to max_gap.py

--- max_gap.py
+++ max_gap.py
@@ -13,3 +13,14 @@ def max_gap(numbers):
 print(max_gap([13, 3, 5]))
 print(max_gap([13,10,2,9,5]))
 print(max_gap([24,299,131,14,26,25]))
+
+
+#User submitted solutions
+""" def max_gap(numbers):
+    lst = sorted(numbers)
+    return max(b-a for a,b in zip(lst, lst[1:])) """
+
+
+""" import numpy
+def max_gap(numbers):
+    return max(numpy.diff(sorted(numbers))) """
