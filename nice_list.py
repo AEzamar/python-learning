eed19ebaedb209d6ad3edaa2b83adcadba509f60
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-24 13:22:32 -0500

    Learning to use map and filter now

--- nice_list.py
+++ nice_list.py
@@ -5,6 +5,8 @@ def is_nice(arr):
             nice_count += 1
         elif num - 1 in arr:
             nice_count += 1
+        else:
+            return False
     return True if nice_count == len(arr) else False
 
 
