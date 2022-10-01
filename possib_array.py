8972876fe427ae58e8e8c3a73ce23f33269e706e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-01 09:04:00 -0500

    & C:/Python310/python.exe e:/Github/Repositories/python-learning/possib_array.py

--- possib_array.py
+++ possib_array.py
@@ -1,11 +1,11 @@
 def is_all_possibilities(arr = []):
     arr.sort()
     for i in arr:
-        if arr[i] == i:
+        if i in arr[i]:
             return True
         else:
             return False
 
 
 print(is_all_possibilities([1, 3, 2, 0]))
-print()
+print(is_all_possibilities([4, 2, 1, 0]))
