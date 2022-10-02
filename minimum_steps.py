24fd6d428a5cdcb92ea8f9f5dc7afc864a0ca8c5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 10:01:34 -0500

    I'm not making progress

--- minimum_steps.py
+++ minimum_steps.py
@@ -5,18 +5,21 @@ def minimum_steps(numbers, value):
     unique_numbers = list(set(numbers))
     numbers.sort()
     print(numbers)
-    for i in range(len(numbers)):
+    """ for i in range(len(numbers)):
         steps += 1
         total += numbers[i] + numbers[i + 1]
         del numbers[0]
         if total > value:
-            break
-    """ while total <= value:
+            break """
+
+    while total < value:
         steps += 1
         total += numbers[0] + numbers[1]
-        del numbers[0]
+        del numbers[0], numbers[1]
+        if len(numbers) == 1:
+            total += numbers[0]
         print(numbers)
-        print(total) """
+        print(total)
     return steps
 
 
