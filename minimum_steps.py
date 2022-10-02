b27d3bd8bd202a07b1fd5de711556ffed6abe44e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 09:14:28 -0500

    Had to sort the array so I could get the minimum values in order, otherwise it messes with the operation

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,7 +1,8 @@
 def minimum_steps(numbers, value):
     total = 0
     steps = 0
-    while total <= value:
+    numbers.sort()
+    while total < value:
         total += numbers[0] + numbers[1]
         del numbers[0]
         steps += 1
