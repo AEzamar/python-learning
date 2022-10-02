a94c3f4d6637b070959c966acf34529f810e4a25
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 09:29:17 -0500

    Getting a bit stuck on this, I'm not getting te correct amount of steps from the funciton

--- minimum_steps.py
+++ minimum_steps.py
@@ -2,13 +2,13 @@ def minimum_steps(numbers, value):
     total = 0
     steps = 0
     unique_numbers = list(set(numbers))
-    print(unique_numbers)
-    unique_numbers.sort()
+    numbers.sort()
+    print(numbers)
     while total < value:
-        total += unique_numbers[0] + unique_numbers[1]
-        del unique_numbers[0]
+        total += numbers[0] + numbers[1]
+        del numbers[0]
         steps += 1
-    print(total)
+        print(total)
     return steps
 
 
