94ac9e7d25953592dd1ab18346de250715e89c4d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 09:24:09 -0500

    Still working on this challenge, the repeated numbers are giving me trouble

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,14 +1,18 @@
 def minimum_steps(numbers, value):
     total = 0
     steps = 0
-    numbers.sort()
+    unique_numbers = list(set(numbers))
+    print(unique_numbers)
+    unique_numbers.sort()
     while total < value:
-        total += numbers[0] + numbers[1]
-        del numbers[0]
+        total += unique_numbers[0] + unique_numbers[1]
+        del unique_numbers[0]
         steps += 1
+    print(total)
     return steps
 
 
 print(minimum_steps([4,6,3], 7))
 print(minimum_steps([10,9,9,8], 17))
 print(minimum_steps([8,9,10,4,2], 23))
+print(minimum_steps([19,98,69,28,75,45,17,98,67], 464))
