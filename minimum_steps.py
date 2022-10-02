678702a0649611ecdddac8fb9ed95b7a22dcb792
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 09:06:19 -0500

    This seems to be working, going to add more test cases

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,9 +1,11 @@
 def minimum_steps(numbers, value):
     total = 0
     steps = 0
-    while total < value:
+    while total <= value:
         total += numbers[0] + numbers[1]
         del numbers[0]
         steps += 1
     return steps
-    
+
+
+print(minimum_steps([4,6,3], 7))
