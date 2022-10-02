d9a35553caccfa419e601cd222769bc9bfa1ad28
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-02 09:02:23 -0500

    Solving this challenge with a while loop

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,4 +1,9 @@
 def minimum_steps(numbers, value):
     total = 0
+    steps = 0
     while total < value:
-        
+        total += numbers[0] + numbers[1]
+        del numbers[0]
+        steps += 1
+    return steps
+    
