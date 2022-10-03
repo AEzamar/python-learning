8ba0f7f0ee4b854ebe8553bf34331f621602c7be
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-03 10:19:05 -0500

    Still working on this challenge, using a for loop to see if I get better results

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,15 +1,19 @@
-from functools import reduce
-def ltd_redux(total, next):
-    
+from functools import reduce    
 def minimum_steps(numbers, value):
     total = 0
     steps = 0
-    numbers.sort()
+    for num in numbers:
+        total += num
+        steps += 1
+        if total >= value:
+            break
+    return steps
+    """ numbers.sort()
     while total < value and len(numbers) > 1:
         total += numbers[0]
         del numbers[0]
         steps += 1
-    return steps
+    return steps """
 
 
 print(minimum_steps([4,6,3], 7))
