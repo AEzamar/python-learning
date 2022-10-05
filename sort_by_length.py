ec47bc5014a89ec01f3a16a7608f8d3c1e5ab886
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-05 12:28:43 -0500

    Went back to the regular syntax with sort

--- sort_by_length.py
+++ sort_by_length.py
@@ -1,6 +1,6 @@
 def sort_by_length(arr):
-    """ arr.sort(key = lambda item: len(item))
-    return arr """
+    arr.sort(key = lambda item: len(item))
+    return arr
 
 
 print(sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]))
