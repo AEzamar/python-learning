3f6cf8df4c4ead5e983216a091d1320f96173f47
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-05 12:14:57 -0500

    I keep forgetting that sort cannot by returned directly or stored in a variable

--- sort_by_length.py
+++ sort_by_length.py
@@ -1,5 +1,6 @@
 def sort_by_length(arr):
-    return arr.sort(key = lambda item: len(item))
+    arr.sort(key = lambda item: len(item))
+    return arr
 
 
 print(sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]))
