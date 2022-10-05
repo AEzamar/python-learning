afe7cf1597e7bdfb719dfea76a68e085ede3ed5f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-05 13:36:44 -0500

    Added user submitted solution

--- sort_by_length.py
+++ sort_by_length.py
@@ -1,6 +1,12 @@
 def sort_by_length(arr):
     arr.sort(key = lambda item: len(item))
-    #return arr
+    return arr
+
+
+#User submitted solution
+#I keep forgetting about sorted
+""" def sort_by_length1(arr):
+    return sorted(arr, key = len) """
 
 
 print(sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]))
