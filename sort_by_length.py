2c7746922cb08abab2197640840d06518cdd8676
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-05 10:09:16 -0500

    Still working on this challenge

--- sort_by_length.py
+++ sort_by_length.py
@@ -1,5 +1,5 @@
 def sort_by_length(arr):
-    return arr.sort(key = lambda a: len(a))
+    return arr.sort(key = lambda a, b: len(a) - len(b))
 
 
 print(sort_by_length(["Telescopes", "Glasses", "Eyes", "Monocles"]))
