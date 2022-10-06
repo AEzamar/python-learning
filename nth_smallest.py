59804568f1c729fbc3a19b442cc6bea90ae0d410
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-06 12:32:52 -0500

    Still figuring this challenge out

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,3 +1,8 @@
 def nth_smallest(arr, pos):
-    sorted_arr = sorted(arr)
-    return sorted_arr[pos]
+    """sorted_arr = sorted(arr)
+    return""" 
+    return [i + 1 for i in range(len(sorted(arr)))]
+
+
+print(nth_smallest([3,1,2], 2))
+print(nth_smallest([15,20,7,10,4,3], 3))
