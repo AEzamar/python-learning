37f6648c13e126af48c51ff52e9690e12bf072ce
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-06 06:40:54 -0500

    Function is working now, going to submit

--- even_numbers_list.py
+++ even_numbers_list.py
@@ -3,4 +3,6 @@ def even_numbers(arr, n):
 
 
 print(even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
+print(even_numbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
+print(even_numbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
 
