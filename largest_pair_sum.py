61651f4d18c4c934d054ef165362fe88dbf6e62d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-06 07:34:19 -0500

    I'm having trouble solving this in a single line

--- largest_pair_sum.py
+++ largest_pair_sum.py
@@ -1,5 +1,6 @@
 def largest_pair_sum(numbers):
     #sorted(numbers, reverse=True)
-    return [num for num in sorted(numbers, reverse=True)] 
+    return [num for num in sorted(numbers, reverse=True)] and sum(numbers[0:2])
+
 
 print(largest_pair_sum([10,14,2,23,19]))
