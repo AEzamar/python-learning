f7021cd0b6cdb3e65f128a29d3a2aad8f37448cd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-06 06:46:56 -0500

    Added user submitted solution to even_numbers_list.py script

--- even_numbers_list.py
+++ even_numbers_list.py
@@ -6,3 +6,9 @@ print(even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 print(even_numbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
 print(even_numbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
 
+
+#User submitted solution
+#I forgot to use list comprehension on this challenge
+""" def even_numbers(arr, n):
+    return list(filter(lambda ele: ele % 2 == 0, arr))[-n:] """
+
