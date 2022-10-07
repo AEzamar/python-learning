b4239d28922705687aedebe178aa018ed3b3fb13
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 07:00:41 -0500

    Going to research how to do non-zero indexing in Python

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,8 +1,9 @@
 def nth_smallest(arr, pos):
     #return [arr[i] for i in range(len(sorted(arr))) if arr[i + 1] == arr[pos]]
     #sorted_arr = sorted(arr)
-    for i in range(len(sorted(arr)) + 1):
-        if sorted(arr)[i + 1] == sorted(arr)[pos]: return sorted(arr)[i]
+    return sorted(arr)[pos - 1]
+    """ for i in range(len(sorted(arr)) + 1):
+        if sorted(arr)[i + 1] == sorted(arr)[pos]: return sorted(arr)[i] """
 
 
 print(nth_smallest([3,1,2], 2))
