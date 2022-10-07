3cf4f678262014a44b051e3939487c3ac2db38d0
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 13:34:34 -0500

    Added a user submitted solution to nth_smallest.py

--- nth_smallest.py
+++ nth_smallest.py
@@ -5,3 +5,10 @@ def nth_smallest(arr, pos):
 print(nth_smallest([3,1,2], 2))
 print(nth_smallest([15,20,7,10,4,3], 3))
 print(nth_smallest([-5,-1,-6,-18], 4))
+
+
+#User submitted solution
+#Never used this module before, have to look it up
+""" from heapq import nthsmallest
+def nth_smallest(arr, pos):
+    return nthsmallest(pos, arr)[-1] """
