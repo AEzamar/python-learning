4d5e3ca6566f51598d1c799a629d4dd7a344988c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 06:47:16 -0500

    Trying to solve this with list comprehension

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,5 +1,6 @@
 def nth_smallest(arr, pos):
-    #return [arr[i] for i in range(len(arr)) if arr[i + 1] == arr[pos]]
+    #sorted_arr = sorted(arr)
+    return [arr[i] for i in range(len(sorted(arr))) if arr[i + 1] == arr[pos]]
     sorted_arr = sorted(arr)
     for i in range(len(sorted_arr) + 1):
         #print('Index of 1',sorted_arr[i + 1])
@@ -7,5 +8,5 @@ def nth_smallest(arr, pos):
             return sorted_arr[i]
 
 
-print(nth_smallest([3,1,2], 3))
+print(nth_smallest([3,1,2], 2))
 print(nth_smallest([15,20,7,10,4,3], 3))
