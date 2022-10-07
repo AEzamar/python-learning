72f7d3e02b9b7b53da9b516bfc4af2dac9761f31
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 07:04:54 -0500

    Yup, this is the soluion to the challenge, submitted and completed

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,10 +1,5 @@
 def nth_smallest(arr, pos):
-    #return [arr[i] for i in range(len(sorted(arr))) if arr[i + 1] == arr[pos]]
-    #sorted_arr = sorted(arr)
-    print(sorted(arr))
     return sorted(arr)[pos - 1]
-    """ for i in range(len(sorted(arr)) + 1):
-        if sorted(arr)[i + 1] == sorted(arr)[pos]: return sorted(arr)[i] """
 
 
 print(nth_smallest([3,1,2], 2))
