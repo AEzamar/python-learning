a508d9222078da4168e3f39467ea7cb2d239f9eb
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 06:41:23 -0500

    I'm having trouble geeting the last element, I get an out of range error

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,13 +1,11 @@
 def nth_smallest(arr, pos):
     #return [arr[i] for i in range(len(arr)) if arr[i + 1] == arr[pos]]
     sorted_arr = sorted(arr)
-    for i in range(len(sorted_arr)):
+    for i in range(len(sorted_arr) + 1):
         #print('Index of 1',sorted_arr[i + 1])
         if sorted_arr[i + 1] == sorted_arr[pos]:
             return sorted_arr[i]
-    #print(sorted_arr[pos])
-    #return [i + 1 for i in range(len(sorted(arr)))]
 
 
-print(nth_smallest([3,1,2], 2))
+print(nth_smallest([3,1,2], 3))
 print(nth_smallest([15,20,7,10,4,3], 3))
