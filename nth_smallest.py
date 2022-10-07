261d54940f1751822993f129b18d1a3f3123b43c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 06:52:06 -0500

    I refactored the function to make it as brief and quick as possible

--- nth_smallest.py
+++ nth_smallest.py
@@ -1,11 +1,8 @@
 def nth_smallest(arr, pos):
+    #return [arr[i] for i in range(len(sorted(arr))) if arr[i + 1] == arr[pos]]
     #sorted_arr = sorted(arr)
-    return [arr[i] for i in range(len(sorted(arr))) if arr[i + 1] == arr[pos]]
-    sorted_arr = sorted(arr)
-    for i in range(len(sorted_arr) + 1):
-        #print('Index of 1',sorted_arr[i + 1])
-        if sorted_arr[i + 1] == sorted_arr[pos]:
-            return sorted_arr[i]
+    for i in range(len(sorted(arr))):
+        if sorted(arr)[i + 1] == sorted(arr)[pos]: return sorted(arr)[i]
 
 
 print(nth_smallest([3,1,2], 2))
