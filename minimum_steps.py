6ce234331e67ce4635a3d5f3560a4d035a4b59e4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-08 09:54:09 -0500

    Still working on minimum_steps.py

--- minimum_steps.py
+++ minimum_steps.py
@@ -39,4 +39,12 @@ print(minimum_steps1([19,98,69,28,75,45,17,98,67], 464))
 def minimum_steps2(numbers, value):
     steps = 0
     sum = numbers[0] + numbers[1]
-    if sum >= value: return 1 
+    if sum < value:
+        steps += 1
+        del numbers[0]
+        del numbers[1]
+        sum += numbers[0]
+    return steps
+
+
+print(minimum_steps2([8,9,10,4,2], 23))
