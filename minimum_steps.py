857e2c25e9579dd438b78104ca408ba2d0fdcf9f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-09 10:20:16 -0500

    Cleaned up the script a bit

--- minimum_steps.py
+++ minimum_steps.py
@@ -1,56 +1,4 @@
 def minimum_steps(numbers, value):
-    total = numbers.pop(0)
-    steps = 0
-    while len(numbers):
-        total += numbers[0]
-        del numbers[0]
-        #print(numbers)
-        steps += 1
-        #print(steps)
-        if total >= value:
-            break
-    return steps
-
-
-""" print(minimum_steps([4,6,3], 7))
-print(minimum_steps([10,9,9,8], 17))
-print(minimum_steps([8,9,10,4,2], 23))
-print(minimum_steps([19,98,69,28,75,45,17,98,67], 464)) """
-
-
-def minimum_steps1(numbers, value):
-    total = numbers.pop(0)
-    steps = 0
-    print('Initial total', total)
-    while total < value:
-        total += numbers.pop(0)
-        steps += 1
-        if total >= value:
-            return steps
-    #return steps
-
-
-""" print(minimum_steps1([4,6,3], 7))
-print(minimum_steps1([10,9,9,8], 17))
-print(minimum_steps1([8,9,10,4,2], 23))
-print(minimum_steps1([19,98,69,28,75,45,17,98,67], 464)) """
-
-
-def minimum_steps2(numbers, value):
-    steps = 0
-    sum = numbers[0] + numbers[1]
-    while sum < value and len(sorted(numbers)) > 1:
-        del numbers[0]
-        del numbers[1]
-        sum += numbers[0]
-        steps += 1
-    return steps
-
-
-#print(minimum_steps2([8,9,10,4,2], 23))
-
-
-def minimum_steps3(numbers, value):
     steps = 0
     sorted_numbers = sorted(numbers)
     total = sorted_numbers[0]
@@ -61,8 +9,8 @@ def minimum_steps3(numbers, value):
     return steps
 
 
-print(minimum_steps3([4,6,3], 7))
-print(minimum_steps3([10,9,9,8], 17))
-print(minimum_steps3([8,9,10,4,2], 23))
-print(minimum_steps3([19,98,69,28,75,45,17,98,67], 464))
-print(minimum_steps3([4,6,3], 2))
+print(minimum_steps([4,6,3], 7))
+print(minimum_steps([10,9,9,8], 17))
+print(minimum_steps([8,9,10,4,2], 23))
+print(minimum_steps([19,98,69,28,75,45,17,98,67], 464))
+print(minimum_steps([4,6,3], 2))
