15edceed2b4418a26e6ed1fd55a21015f7dcc1b7
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-10 06:22:04 -0500

    I really don't remember what to do in recursive_reduction.py, I have to look up the challenge again and look up the concept of root in math

--- recursive_reduction.py
+++ recursive_reduction.py
@@ -3,7 +3,7 @@ def digital_root(n):
     n_list = list(map(int, list(str(n))))
     print(n_list)
     for digit in range(len(n_list)):
-        total += sum(int(digit))
+        total += digit
     return total
 
 
