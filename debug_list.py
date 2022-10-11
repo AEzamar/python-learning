993709cca7f7bc0f2fd05afc2fabc039a614f8c2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 06:58:04 -0500

    Added a user submmitted solution to debug_list.py

--- debug_list.py
+++ debug_list.py
@@ -8,3 +8,9 @@ print(without_last([1, 2, 3, 4, 5]))
 print(without_last([9, 7, 6, 9]))
 print(without_last([25, 30, 55, 26, 66, 59, 94, 85, 8]))
 print(without_last([70, 43, 41, 92]))
+
+
+#User submitted solution
+#Come on!
+""" def without_last(lst):
+    return lst[:-1] """
