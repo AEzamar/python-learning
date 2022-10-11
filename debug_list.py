17121d3c0a6b9819d1749642153d20e8ea1a3eae
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 06:48:30 -0500

    I'm testing this function and it works fine

--- debug_list.py
+++ debug_list.py
@@ -5,3 +5,5 @@ def without_last(lst):
 
 print(without_last([1, 2, 3, 4, 5]))
 print(without_last([9, 7, 6, 9]))
+print(without_last([25, 30, 55, 26, 66, 59, 94, 85, 8]))
+print(without_last([70, 43, 41, 92]))
