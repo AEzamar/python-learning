78114b5abb09ed48d65579c00f02904d265779df
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 07:10:15 -0500

    I'm getting the elements in the diagonal, now I need to modify depending on if they're negative or positive

--- two_dimesional_list.py
+++ two_dimesional_list.py
@@ -1,4 +1,15 @@
-def matrix(array): 
+def matrix(array):
+    indexer = 0 
     for i in range(len(array)):
-        for j in range(len(array)):
-            
+        print(array[indexer][i])
+        indexer += 1
+        """ for j in range(len(array)):
+            print(array[j][i]) """
+    return array
+
+
+print(matrix([[-1, 4, -5, -9, 3], 
+            [6, -4, -7, 4, -5], 
+            [3, 5, 4, -9, -1], 
+            [1, 5, -7, -8, -9], 
+            [-3, 2, 1, -5, 6]]))
