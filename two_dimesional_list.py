fd1c2123d4ace366fbdb74fbe21f780e1cd2b1d4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 12:29:28 -0500

    The ternary operator syntax was not working

--- two_dimesional_list.py
+++ two_dimesional_list.py
@@ -1,7 +1,6 @@
 def matrix(array):
     indexer = 0 
     for i in range(len(array)):
-        #array[indexer][i] = 0 if array[indexer][i] < 0 else array[indexer][i] = 1 
         if array[indexer][i] < 0: array[indexer][i] = 0
         else: array[indexer][i] = 1  
         indexer += 1
