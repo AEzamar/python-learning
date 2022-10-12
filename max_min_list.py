f8653f3a5fbdaddc8ba46d63eb404f2b35ef7356
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 07:27:07 -0500

    I need to remove the first and the last element of the list to get the max and min sorting order in the output list

--- max_min_list.py
+++ max_min_list.py
@@ -1,11 +1,10 @@
-import math
 def solve(arr):
     arr_copy = arr.copy()
     min_max_arr = []
     while len(sorted(arr_copy, reverse=True)):
-        print(math.max(arr_copy))
-        arr_copy.pop(0)
-    return arr
+        min_max_arr.append(arr_copy.pop(0))
+        #arr_copy.pop(0)
+    return arr_copy
 
 
 print(solve([15, 12, 10, 11, 7]))
