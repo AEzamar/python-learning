8c4bca405956e86cec1c8aa61c71bf55898928d9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 09:45:16 -0500

    & C:/Python310/python.exe e:/Github/Repositories/python-learning/max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -3,8 +3,9 @@ def solve(arr):
     max_min_arr = []
     while len(sorted(arr_copy, reverse=True)):
         max_min_arr.append(arr_copy.pop(0))
+        del arr_copy[:-1]
         #arr_copy.pop(0)
-    return arr_copy
+    return max_min_arr
 
 
 print(solve([15, 12, 10, 11, 7]))
