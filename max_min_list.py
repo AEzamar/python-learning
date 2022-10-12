f1686ae06adf9d18733aa407f233fcd9a8f04553
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 10:04:45 -0500

    I tried appending the first and last element in the array while deleting them from the copy of the input array but I get errors and can't populate the ouput array

--- max_min_list.py
+++ max_min_list.py
@@ -2,9 +2,10 @@ def solve(arr):
     arr_copy = arr.copy()
     max_min_arr = []
     while len(sorted(arr_copy, reverse=True)):
-        max_min_arr.append(arr_copy.pop(0))
-        max_min_arr.append(arr_copy[-1])
-        del arr_copy[:-1]
+        #print(max(arr_copy))
+        arr_copy.pop(0)
+        """ max_min_arr.append(arr_copy.pop(0))
+        max_min_arr.append(arr_copy.pop()) """
         #arr_copy.pop(0)
     return max_min_arr
 
