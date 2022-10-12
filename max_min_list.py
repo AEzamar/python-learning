97ef95d79fd10c305ded2cf7475811901c4d6db1
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 09:46:31 -0500

    I'm struggling with getting the max and min numbers from the list and into the ouput list in max-min order

--- max_min_list.py
+++ max_min_list.py
@@ -3,6 +3,7 @@ def solve(arr):
     max_min_arr = []
     while len(sorted(arr_copy, reverse=True)):
         max_min_arr.append(arr_copy.pop(0))
+        max_min_arr.append(arr_copy[-1])
         del arr_copy[:-1]
         #arr_copy.pop(0)
     return max_min_arr
