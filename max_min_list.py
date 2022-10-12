8a4fd07e850530e25b052e73b8b465dde58bb586
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 13:34:47 -0500

    I still can't remove the last element from the copy array, I get out of range errors

--- max_min_list.py
+++ max_min_list.py
@@ -1,12 +1,14 @@
 def solve(arr):
     arr_copy = arr.copy()
     max_min_arr = []
-    while len(sorted(arr_copy, reverse=True)):
-        #print(max(arr_copy))
-        arr_copy.pop(0)
+    #while len(sorted(arr_copy, reverse=True)):
+    for i in range(len(arr_copy)):
+        max_min_arr.append(arr_copy[0])
+        max_min_arr.append(arr_copy[-1])
+        del arr_copy[0]
+        del arr_copy[-1]
         """ max_min_arr.append(arr_copy.pop(0))
         max_min_arr.append(arr_copy.pop()) """
-        #arr_copy.pop(0)
     return max_min_arr
 
 
