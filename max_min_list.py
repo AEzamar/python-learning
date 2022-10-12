af068cc5df96b23e36df862ec9de026588b27b8a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 07:15:19 -0500

    Testing the math module in the script max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -1,7 +1,13 @@
+from distutils.spawn import spawn
 import math
 def solve(arr):
     arr_copy = arr.copy()
     min_max_arr = []
     while len(sorted(arr_copy, reverse=True)):
         print(math.max(arr_copy))
+        arr_copy.pop(0)
+    return arr
+
+
+print(solve(15, 12, 10, 11, 7))
         
