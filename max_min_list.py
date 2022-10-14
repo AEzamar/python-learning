ee08cee33bda61bbd2455a381b0c3009593c4806
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-14 07:24:20 -0500

    I'm trying to get the last element from another copy of the original array to get the output array populated

--- max_min_list.py
+++ max_min_list.py
@@ -1,11 +1,13 @@
 def solve(arr):
     arr_copy = arr.copy()
+    arr_copy_two = arr.copy()
+    arr_copy_two.sort()
     max_min_arr = []
     while len(sorted(arr_copy, reverse=True)):
         max_min_arr.append(arr_copy[0])
-        max_min_arr.append(arr_copy[-1])
+        max_min_arr.append(arr_copy_two[-1])
         del arr_copy[0]
-        del arr_copy[-1]
+        #del arr_copy[-1]
     return max_min_arr
 
 
