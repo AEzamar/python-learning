70157074bf5bcfaa430d41308159dd3e7193cd8f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-14 09:41:13 -0500

    I've been having trouble with this for days

--- max_min_list.py
+++ max_min_list.py
@@ -1,6 +1,10 @@
 def solve(arr):
-    return [num for num in arr]
-    arr_copy = arr.copy()
+    sorted_arr = sorted(arr, reverse=True)
+    max_min_arr = []
+    for i in range(len(sorted_arr)):
+        max_min_arr.append(sorted_arr[i])
+        max_min_arr.append(sorted_arr[-i])
+    """ arr_copy = arr.copy()
     arr_copy_two = arr.copy()
     arr_copy_two.sort()
     max_min_arr = []
@@ -9,7 +13,8 @@ def solve(arr):
         max_min_arr.append(arr_copy[0])
         max_min_arr.append(arr_copy_two[0])
         del arr_copy[0]
-        del arr_copy_two[0]
+        del arr_copy_two[0] """
+
     return max_min_arr
 
 
