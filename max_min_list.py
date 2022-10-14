4face802c37235b3399b3f4b9dda569f2c19e315
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-14 06:28:05 -0500

    The challenge from max_min_list.py hasn't been solved yet

--- max_min_list.py
+++ max_min_list.py
@@ -4,8 +4,7 @@ def solve(arr):
     while len(sorted(arr_copy, reverse=True)):
         max_min_arr.append(arr_copy[0])
         max_min_arr.append(arr_copy[-1])
-        del arr_copy[0]
-        #del arr_copy[-1]
+        del arr_copy[0] and del arr_copy[-1]
     return max_min_arr
 
 
