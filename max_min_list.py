36324b19374efd4a7bafce9ba9cdbb1c8a0ce55d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-15 08:47:01 -0500

    My plan was to convert the output array to a set right at the return line and the convert that set of unique elements to a list with the list function, but converting the array to a set changes the order of the elements

--- max_min_list.py
+++ max_min_list.py
@@ -1,15 +1,10 @@
 def solve(arr):
-    iteration_arr = arr.copy()
     max_arr = sorted(arr, reverse=True)
     min_arr = sorted(arr)
     max_min_arr = []
-    index = 0
-    #for i in range(len(arr)):
-    while(len(iteration_arr)):
-        max_min_arr.append(max_arr[index])
-        max_min_arr.append(min_arr[index])
-        index += 1
-        del iteration_arr[0]
+    for i in range(len(arr)):
+        max_min_arr.append(max_arr[i])
+        max_min_arr.append(min_arr[i])
     return max_min_arr
 
 
