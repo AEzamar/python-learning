f84e0996d3ec66b8e4ab301047f8d492397c3cd0
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-16 12:00:11 -0500

    Had to go back to appending elements on max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -3,8 +3,8 @@ def solve(arr):
     min_arr = sorted(arr)
     max_min_arr = []
     for i in range(len(arr)):
-        max_min_arr += max_arr[i]
-        max_min_arr += min_arr[i]
+        max_min_arr.append(max_arr[i])
+        max_min_arr.append(min_arr[i])
     return max_min_arr
 
 
