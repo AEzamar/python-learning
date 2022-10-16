b63f1fde5fd74e8329476c391f721b45a2f4d9c6
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-16 09:10:22 -0500

    Testing this script with basic test cases

--- list_parity.py
+++ list_parity.py
@@ -1,8 +1,8 @@
 def solve(arr):
     positive_lst = sorted(filter(lambda item: item > 0, arr))
     negative_lst = sorted(filter(lambda item: item < 0, arr))
-    print(positive_lst)
-    print(negative_lst)
-
+    for i in range(len(arr)):
+        if -int(positive_lst[i]) not in negative_lst:
+            return positive_lst[i]
 
 print(solve([1, -1, 2, -2, 3]))
