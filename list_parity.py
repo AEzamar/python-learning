ab48619f1b3c46e66969a7ace5e55b1be3d9c733
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-17 06:32:15 -0500

    The abs method is not working, I get an index out of range error

--- list_parity.py
+++ list_parity.py
@@ -2,7 +2,7 @@ def solve(arr):
     positive_lst = sorted(filter(lambda item: item > 0, arr))
     negative_lst = sorted(filter(lambda item: item < 0, arr))
     for i in range(len(arr)):
-        if -int(positive_lst[i]) not in negative_lst:
+        if -abs(positive_lst[i]) not in negative_lst:
             return positive_lst[i]
 
 
