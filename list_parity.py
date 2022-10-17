6b94052a9c0d99fac4ae22d46151fbb32ff7da73
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-17 06:56:57 -0500

    This syntax is sort of working, but I get the wrong result in the third test case

--- list_parity.py
+++ list_parity.py
@@ -2,8 +2,8 @@ def solve(arr):
     positive_lst = sorted(filter(lambda item: item > 0, arr))
     negative_lst = sorted(filter(lambda item: item < 0, arr))
     for i in range(len(arr)):
-        if -abs(positive_lst[i]) not in negative_lst:
-            return positive_lst[i]
+        if -abs(positive_lst[i]) not in negative_lst or abs(negative_lst[i]) not in positive_lst:
+            return positive_lst[i] or negative_lst[i]
 
 
 print(solve([1, -1, 2, -2, 3]))
