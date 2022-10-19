1a912a0c5ae5c5c9a6e8a4ff7f5e0c8174766afd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-18 21:45:07 -0500

    Working a little bit on max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -1,14 +1,14 @@
 def solve(arr):
     max_arr = sorted(arr, reverse=True)
     min_arr = sorted(arr)
-    copy_arr = sorted(arr, reverse=True)
+    copy_arr = sorted(arr)
     max_min_arr = []
     for i in range(len(copy_arr)):
     #while len(copy_arr):
         if copy_arr[0] and copy_arr[-1] not in max_min_arr:
-            max_min_arr.append(copy_arr[0])
+            max_min_arr.append(max_arr[0])
             del copy_arr[0]
-            max_min_arr.append(copy_arr[-1])
+            max_min_arr.append(min_arr[-1])
             del copy_arr[-1]
     return max_min_arr
 
