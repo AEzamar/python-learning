16d44dc15380c1e4961b7ece315f104f8ea690ab
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-19 06:18:53 -0500

    Trying to iterate with a separate list and taking to append elements from different lists into output list

--- max_min_list.py
+++ max_min_list.py
@@ -3,9 +3,8 @@ def solve(arr):
     min_arr = sorted(arr)
     copy_arr = sorted(arr)
     max_min_arr = []
-    for i in range(len(copy_arr)):
-    #while len(copy_arr):
-        if copy_arr[0] and copy_arr[-1] not in max_min_arr:
+    #for i in range(len(copy_arr)):
+    while len(copy_arr):
             max_min_arr.append(max_arr[0])
             max_min_arr.append(min_arr[0])
             del copy_arr[0]
