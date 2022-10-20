5a5ad1fc3d92ffc6c9b1dbcd52ba83c2356f314d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-20 07:16:30 -0500

    This script is working for some cases but with others I'm getting an index out of range error

--- list_parity.py
+++ list_parity.py
@@ -4,13 +4,14 @@ def solve(arr):
     for i in range(len(arr)):
         if -abs(positive_lst[i]) not in negative_lst:
             return positive_lst[i]
-        elif abs(negative_lst[i]) not in positive_lst:
+        if abs(negative_lst[i]) not in positive_lst:
             return negative_lst[i]
 
 
 print(solve([1, -1, 2, -2, 3]))
 print(solve([1, 3, 2, -1, -3]))
 print(solve([-3, 1, 2, 3, -1, -4, -2]))
+print(solve([1, -1, 2, -2, 3, 3]))
 
 """ def solve1(arr):
     return [-abs(ele) not in arr for ele in arr] """
