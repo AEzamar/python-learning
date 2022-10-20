3a73a7017bc18eb0629b4b3428087b2bdb3c5bcf
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-20 06:51:55 -0500

    I solved this challenge in JS, now I'm trying to solve it with Python

--- list_parity.py
+++ list_parity.py
@@ -6,6 +6,12 @@ def solve(arr):
             return positive_lst[i] or negative_lst[i]
 
 
-print(solve([1, -1, 2, -2, 3]))
+""" print(solve([1, -1, 2, -2, 3]))
 print(solve([1, 3, 2, -1, -3]))
-print(solve([-3, 1, 2, 3, -1, -4, -2]))
+print(solve([-3, 1, 2, 3, -1, -4, -2])) """
+
+def solve1(arr):
+    return [(-abs(ele) not in arr) for ele in arr]
+
+
+print(solve1([1, -1, 2, -2, 3]))
