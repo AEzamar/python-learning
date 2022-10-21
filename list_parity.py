375ca60ca5ba0ed7065cff44f0e11d2864670024
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-21 06:43:34 -0500

    I submitted solve function and it seems to be working, the code looks kind of ugly

--- list_parity.py
+++ list_parity.py
@@ -15,6 +15,7 @@ print(solve([1, 3, 2, -1, -3]))
 print(solve([-3, 1, 2, 3, -1, -4, -2]))
 print(solve([1, -1, 2, -2, 3, 3]))
 
+
 def solve1(arr):
     #return list(filter(lambda ele: ele != -abs(ele), arr))
     return [-abs(ele) not in arr for ele in arr]
