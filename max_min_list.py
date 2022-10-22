89237516b352a99042d755ae9d96112f0c09e9c4
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-22 15:11:08 -0500

    Just tried some small changes to see if it affects the output list

--- max_min_list.py
+++ max_min_list.py
@@ -9,7 +9,7 @@ def solve(arr):
         max_min_arr.append(min(min_arr))
         del min_arr[0]
         del copy_arr[0]
-        if len(max_min_arr) >= len(arr): return max_min_arr
+        if len(max_min_arr) >= len(arr): break
     return max_min_arr
 
 
