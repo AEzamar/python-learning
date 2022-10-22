34b20e6b8d9ba69b8ab902e90efa39ddca408511
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-22 09:58:28 -0500

    Back to trying to solve max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -6,8 +6,12 @@ def solve(arr):
     i = 0
     #for i in range(len(copy_arr)):
     while len(copy_arr):
-        max_min_arr.append(max_arr[i])
-        max_min_arr.append(min_arr[i])
+        """ max_min_arr.append(max_arr[i])
+        max_min_arr.append(min_arr[i]) """
+        print(max(arr))
+        print(min(arr))
+        arr.pop(0)
+        #max_min_arr.append(arr.remove(max(arr)))
         i += 1
         del copy_arr[0]
         if len(max_min_arr) > len(arr): break
