f5e8c69af12b3831ff0b6afe41801d6ce3cd82ff
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-22 14:44:41 -0500

    Still missing the last number of the array

--- max_min_list.py
+++ max_min_list.py
@@ -3,17 +3,11 @@ def solve(arr):
     min_arr = sorted(arr)
     copy_arr = sorted(arr)
     max_min_arr = []
-    i = 0
     while len(copy_arr):
-        """ max_min_arr.append(max_arr[i])
-        max_min_arr.append(min_arr[i]) """
         max_min_arr.append(max(arr))
         arr.pop(0)
         max_min_arr.append(min(min_arr))
         del min_arr[0]
-        #arr.pop()
-        #max_min_arr.append(arr.remove(max(arr)))
-        #i += 1
         del copy_arr[0]
         if len(max_min_arr) >= len(arr): break
     return max_min_arr
