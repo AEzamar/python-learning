06fe36cd35c87cb3997920b08eb567e69e15d223
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-22 15:31:06 -0500

    No real progress on this

--- max_min_list.py
+++ max_min_list.py
@@ -1,15 +1,15 @@
 def solve(arr):
-    max_arr = sorted(arr, reverse=True)
+    max_arr = sorted(arr)
     min_arr = sorted(arr)
     copy_arr = sorted(arr)
     max_min_arr = []
     while len(copy_arr):
-        max_min_arr.append(max(max_arr))
-        del max_arr[0]
-        max_min_arr.append(min(min_arr))
-        del min_arr[0]
+        max_min_arr.append(max(arr))
+        arr.pop(0)
+        max_min_arr.append(min(arr))
+        arr.pop()
         del copy_arr[0]
-        if len(max_min_arr) >= len(arr): break
+        if len(max_min_arr) >= len(arr): return max_min_arr
     return max_min_arr
 
 
