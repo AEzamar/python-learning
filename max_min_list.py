0309e8a273a0454e850ad57fcf0f458976f015ac
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 16:36:02 -0500

    Still can't make it work

--- max_min_list.py
+++ max_min_list.py
@@ -4,10 +4,10 @@ def solve(arr):
     copy_arr = sorted(arr)
     max_min_arr = []
     while len(copy_arr):
-        max_min_arr.append(max(arr))
-        arr.pop(0)
-        max_min_arr.append(min(arr))
-        arr.pop()
+        max_min_arr.append(max(max_arr))
+        max_arr.pop()
+        max_min_arr.append(min(min_arr))
+        min_arr.pop(0)
         del copy_arr[0]
         if len(max_min_arr) >= len(arr): break
     return max_min_arr
