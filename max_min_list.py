d0fe1ddfca3a398028b81e283c9760dbeaba9b92
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-24 18:44:15 -0500

    Still working max_min_list.py

--- max_min_list.py
+++ max_min_list.py
@@ -9,6 +9,7 @@ def solve(arr):
         max_min_arr.append(min(min_arr))
         min_arr.pop(0)
         del copy_arr[0]
+        if len(copy_arr) == 1: max_min_arr(copy_arr.pop())
         if len(max_min_arr) >= len(arr): break
     return max_min_arr
 
