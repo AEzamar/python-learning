14251ded3f5f2fa930ce5da65a75f1955f005fd3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-24 21:22:53 -0500

    Still cleaning up sort_value_index.py

--- sort_value_index.py
+++ sort_value_index.py
@@ -7,9 +7,6 @@ def sort_product(ele, len):
         multiplier += 1
 
 
-def multiply(x, y):
-    return x * y
-
 #sort_by_product = lambda i, ele: ele * (i + 1)
 def sort_by_value_and_index(lst):
     pass
