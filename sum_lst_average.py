da126843197f08e10fda8bff63c90d5a84a6ddf3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-26 06:44:58 -0500

    I need to sum all the elements, get the average of every sub list and then sum those averages

--- sum_lst_average.py
+++ sum_lst_average.py
@@ -1,6 +1,6 @@
 from functools import reduce
 def sum_average(arr):
-    return int([reduce(lambda total, curr: total + curr, subArr) for subArr in arr]) / len(arr)
+    return sum([reduce(lambda total, curr: total + curr, subArr) for subArr in arr]) / len(arr)
 
 
 print(sum_average([[1, 2, 2, 1], [2, 2, 2, 1]]))
