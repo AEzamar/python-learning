872dfeea82210b6874be42daf0e670856dcba962
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-26 06:37:19 -0500

    This is not quite working, I need to use reduce

--- sum_lst_average.py
+++ sum_lst_average.py
@@ -1,6 +1,7 @@
+from functools import reduce
 def sum_average(arr):
     return sum([sum(subArr) for subArr in arr])
 
 
-
 print(sum_average([[1, 2, 2, 1], [2, 2, 2, 1]]))
+print(sum_average([[52, 64, 84, 21, 54], [44, 87, 46, 90, 43]]))
