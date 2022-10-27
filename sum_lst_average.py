4c939bcece179eb5ac75297c368343abff5902c0
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-27 07:31:04 -0500

    I don't know how to compare decimals in floats, and I don't know if it can be done

--- sum_lst_average.py
+++ sum_lst_average.py
@@ -1,6 +1,7 @@
 import math
 def sum_average(arr):
-    return round(sum([sum(subArr) / len(subArr) for subArr in arr]))
+    average_sum = sum([sum(subArr) / len(subArr) for subArr in arr])
+    return ceil(average_sum) if average_sum 
 
 
 print(sum_average([[1, 2, 2, 1], [2, 2, 2, 1]]))
