f394436ebf7fff3440be2f1347771539f5e15f4c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-05 08:39:06 -0600

    This solution didn'tt worked

--- max_min_list.py
+++ max_min_list.py
@@ -14,7 +14,11 @@ def solve(arr):
         max_min_arr.append(min(copy_arr_2))
         copy_arr.pop()
         copy_arr_2.pop(0)
-        if len(max_min_arr) >= len(arr): return max_min_arr
+        if len(copy_arr) == 1:
+            max_min_arr.append(copy_arr.pop())
+        if len(copy_arr_2) == 1:
+            max_min_arr.append(copy_arr_2.pop())
+        #if len(max_min_arr) >= len(arr): return max_min_arr
     return max_min_arr
 
 
