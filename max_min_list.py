66faacbb81f726a91e387e888ed3c61305274f5c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-05 08:28:25 -0600

    More duplicates in output list

--- max_min_list.py
+++ max_min_list.py
@@ -2,6 +2,7 @@ def solve(arr):
     max_arr = sorted(arr)
     min_arr = sorted(arr)
     copy_arr = sorted(arr)
+    copy_arr_2 = sorted(arr)
     max_min_arr = []
     while len(copy_arr):
         """ max_min_arr.append(max(max_arr))
@@ -10,8 +11,9 @@ def solve(arr):
         min_arr.pop(0)
         del copy_arr[0] """
         max_min_arr.append(max(copy_arr))
-        max_min_arr.append(min(copy_arr))
-        copy_arr.pop() and copy_arr.pop(0)
+        max_min_arr.append(min(copy_arr_2))
+        copy_arr.pop()
+        copy_arr_2.pop(0)
         if len(max_min_arr) == len(arr): return max_min_arr
     return max_min_arr
 
